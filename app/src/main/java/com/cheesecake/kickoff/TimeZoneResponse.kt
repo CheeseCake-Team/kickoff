c5596dd9350a1d4ae39a7569c2e61924c60a4061
hassanwasfy <wasfyelgamed@gmail.com>
2023-05-25 03:45:29 +0300

    implement TimezoneResponse

---
+++ app/src/main/java/com/cheesecake/kickoff/TimeZoneResponse.kt
@@ -0,0 +1,17 @@
+package com.cheesecake.kickoff
+import com.google.gson.annotations.SerializedName
+
+data class TimezoneResponse(
+    @SerializedName("get")
+    val get: String,
+    @SerializedName("parameters")
+    val parameters: List<Any>,
+    @SerializedName("errors")
+    val errors: List<Any>,
+    @SerializedName("results")
+    val results: Int,
+    @SerializedName("paging")
+    val paging: Paging,
+    @SerializedName("response")
+    val response: List<String>
+)
