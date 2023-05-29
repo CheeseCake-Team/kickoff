99e45bf4a5ce2f62cbe53d184b571392c366c660
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-05-30 02:07:12 +0300

    implement League Details Fragment

---
+++ app/src/main/java/com/cheesecake/league/LeagueDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.cheesecake.league
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class LeagueDetailsFragment : Fragment() {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
