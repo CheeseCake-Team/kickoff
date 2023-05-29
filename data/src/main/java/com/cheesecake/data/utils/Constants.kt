e81a5e75eff1c8915e06273a66a99b84f71a2c9e
hassanwasfy <wasfyelgamed@gmail.com>
2023-05-30 00:39:59 +0300

    delete Constants.kt

--- data/src/main/java/com/cheesecake/data/utils/Constants.kt
+++ data/src/main/java/com/cheesecake/data/utils/Constants.kt
@@ -1,5 +1,3 @@
 package com.cheesecake.data.utils
 
-const val API_HOST_HEADER = "x-rapidapi-host"
-const val API_HOST_VALUE = "v3.football.api-sports.io"
-const val API_KEY_HEADER = "x-rapidapi-key"
+
