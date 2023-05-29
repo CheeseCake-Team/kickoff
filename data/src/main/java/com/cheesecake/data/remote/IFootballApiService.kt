58fa68ae44d05617b0b6824d6f84826655cd6abd
hassanwasfy <wasfyelgamed@gmail.com>
2023-05-29 17:41:52 +0300

    add api service methods

--- data/src/main/java/com/cheesecake/data/remote/IFootballApiService.kt
+++ data/src/main/java/com/cheesecake/data/remote/IFootballApiService.kt
@@ -1,12 +1,23 @@
 package com.cheesecake.data.remote
 
 import com.cheesecake.data.models.BaseResponse
+import com.cheesecake.data.models.CountriesResponse
+import com.cheesecake.data.models.LeagueResponse
 import com.cheesecake.data.models.TimezoneResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface IFootballApiService {
 
     @GET("timezone")
     suspend fun getAllTimezones():BaseResponse<TimezoneResponse>
+
+    @GET("countries")
+    suspend fun getAllCountries():BaseResponse<CountriesResponse>
+
+    @GET("leagues")
+    suspend fun getLeagueById(@Query("id") id: Int):BaseResponse<LeagueResponse>
+
+
 
 }
