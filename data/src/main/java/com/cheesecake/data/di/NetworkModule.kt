bfa9df594ad6921005093455d8c9c2efe4fac4fd
hassanwasfy <wasfyelgamed@gmail.com>
2023-05-29 14:45:40 +0300

    implement request module

--- data/src/main/java/com/cheesecake/data/di/NetworkModule.kt
+++ data/src/main/java/com/cheesecake/data/di/NetworkModule.kt
@@ -1,11 +1,13 @@
 package com.cheesecake.data.di
 
+import com.cheesecake.data.remote.IFootballApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,6 +17,26 @@ import javax.inject.Singleton
 object NetworkModule {
 
     //TODO write api service provider injection
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofitService(retrofit: Retrofit): IFootballApiService {
+        return retrofit.create(IFootballApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitBuilder(
+        client: OkHttpClient,
+        factory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://v3.football.api-sports.io")
+            .client(client)
+            .addConverterFactory(factory)
+            .build()
+    }
 
     @Singleton
     @Provides
@@ -28,14 +50,12 @@ object NetworkModule {
     }
 
 
-
-
     @Singleton
     @Provides
     fun provideLoggingInterceptor(): HttpLoggingInterceptor =
         HttpLoggingInterceptor().apply {
             setLevel(HttpLoggingInterceptor.Level.BODY)
-    }
+        }
 
     @Singleton
     @Provides
---
+++ data/src/main/java/com/cheesecake/data/remote/IFootballApiService.kt
@@ -0,0 +1,4 @@
+package com.cheesecake.data.remote
+
+interface IFootballApiService {
+}
