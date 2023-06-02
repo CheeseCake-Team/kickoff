70c9c10e6d156100cb1c6fc794a48d4ad17d7265
hassanwasfy <wasfyelgamed@gmail.com>
2023-06-02 11:44:30 +0300

    implement base viewmodel

---
+++ ui/src/main/java/com/cheesecake/ui/base/BaseViewModel.kt
@@ -0,0 +1,17 @@
+package com.cheesecake.ui.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    protected val compositeDisposable = CompositeDisposable()
+
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
