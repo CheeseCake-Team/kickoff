338c858b722b66ed85213bed9262544e3d7068de
Nahla <nenoopa@gmail.com>
2023-06-04 00:27:08 +0300

    enhance HeadToHead DTO

--- data/src/main/java/com/cheesecake/data/models/HeadToHeadDTO.kt
+++ data/src/main/java/com/cheesecake/data/models/HeadToHeadDTO.kt
@@ -1,16 +1,13 @@
 package com.cheesecake.data.models
 
-import com.cheesecake.data.models.base.BasePeriods
-import com.cheesecake.data.models.base.BaseStatus
 import com.cheesecake.data.models.base.Fixture
 import com.cheesecake.data.models.base.LeagueCountrySeasonRound
 import com.cheesecake.data.models.base.Matches
 import com.cheesecake.data.models.base.ScoreBlock
 import com.cheesecake.data.models.base.TeamHomeAway
-import com.cheesecake.data.models.base.VenueCity
 import com.google.gson.annotations.SerializedName
 
-data class HeadToHeadResponse(
+data class HeadToHeadDTO(
     @SerializedName("fixture")
     val fixture: Fixture,
     @SerializedName("league")
