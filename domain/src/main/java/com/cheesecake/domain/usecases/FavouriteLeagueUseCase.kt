ea69ba73a002d70e3cd6a09ccef3d27221b7aa76
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-06 19:35:02 +0300

    implement FavouriteLeagueUseCase

---
+++ domain/src/main/java/com/cheesecake/domain/usecases/FavouriteLeagueUseCase.kt
@@ -0,0 +1,27 @@
+package com.cheesecake.domain.usecases
+
+import com.cheesecake.domain.models.League
+import com.cheesecake.domain.repository.IFootballRepository
+import javax.inject.Inject
+
+class FavouriteLeagueUseCase @Inject constructor(
+    private val footballRepository: IFootballRepository,
+    private val getLeagueByIdAndSeasonUseCase: GetLeagueByIdAndSeasonUseCase
+) {
+
+    suspend operator fun invoke(leagueId: Int, leagueSeason: Int) {
+        getLeagueByIdAndSeasonUseCase(leagueId, leagueSeason)?.let {
+            footballRepository.updateOrInsertLeague(
+                League(
+                    leagueId = it.leagueId,
+                    leagueName = it.leagueName,
+                    leagueLogo = it.leagueLogo,
+                    leagueSeason = it.leagueSeason,
+                    seasonStartYear = it.seasonStartYear,
+                    seasonEndYear = it.seasonEndYear,
+                    isFavourite = !it.isFavourite
+                )
+            )
+        }
+    }
+}
