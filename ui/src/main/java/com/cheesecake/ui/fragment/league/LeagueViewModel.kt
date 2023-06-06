a82cd0436b16f7a9eb4fcc0241f636615237abcd
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-06 18:30:33 +0300

    update LeagueViewModel

--- ui/src/main/java/com/cheesecake/ui/fragment/league/LeagueViewModel.kt
+++ ui/src/main/java/com/cheesecake/ui/fragment/league/LeagueViewModel.kt
@@ -1,6 +1,51 @@
 package com.cheesecake.ui.fragment.league
 
+import androidx.lifecycle.viewModelScope
+import com.cheesecake.domain.usecases.GetLeagueByIdAndSeasonUseCase
 import com.cheesecake.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LeagueViewModel: BaseViewModel() {
+data class LeagueUIState(
+    val errorMessage: String = "error",
+    val isLoading: Boolean = true,
+    val seasonStartEndYear: String = "",
+    val imageUrl: String = "",
+    val leagueName: String = "",
+    val isFavourite: Boolean = false
+)
+
+@HiltViewModel
+class LeagueViewModel @Inject constructor(
+    private val getLeagueByIdAndSeasonUseCase: GetLeagueByIdAndSeasonUseCase
+) : BaseViewModel() {
+
+    private val _leagueUIState = MutableStateFlow(LeagueUIState())
+    val leagueUIState = _leagueUIState.asStateFlow()
+
+    init {
+        getLeague()
+    }
+
+    private fun getLeague() {
+        viewModelScope.launch {
+            getLeagueByIdAndSeasonUseCase(39, 2022)?.let { league ->
+                _leagueUIState.update {
+                    it.copy(
+                        leagueName = league.leagueName,
+                        seasonStartEndYear = "${league.seasonStartYear}/${league.seasonEndYear}",
+                        imageUrl = league.leagueLogo,
+                        isFavourite = league.isFavourite,
+                        isLoading = false
+                    )
+                }
+            }
+
+        }
+    }
+
 }
