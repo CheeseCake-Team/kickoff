5913b1f5a23f55c04e1ff78e2c9c18eae0796f20
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-06 18:28:08 +0300

    update GetLeagueByIdAndSeason rename GetLeagueByIdAndSeason.kt to GetLeagueByIdAndSeasonUseCase

---
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetLeagueByIdAndSeasonUseCase.kt
@@ -0,0 +1,16 @@
+package com.cheesecake.domain.usecases
+
+import com.cheesecake.domain.models.League
+import com.cheesecake.domain.repository.IFootballRepository
+import javax.inject.Inject
+
+class GetLeagueByIdAndSeasonUseCase @Inject constructor(
+    private val footballRepository: IFootballRepository,
+) {
+
+    suspend operator fun invoke(leagueId: Int, leagueSeason: Int): League? {
+        return footballRepository.getLocallyLeagueByIdAndSeason(leagueId, leagueSeason)
+            ?: footballRepository.getRemotelyLeagueByIdAndSeason(leagueId, leagueSeason)
+    }
+
+}
