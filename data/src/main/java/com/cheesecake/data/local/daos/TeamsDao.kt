ed81dec62a205677fff7d9d13dd0e323b64a3bbc
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-06 14:55:42 +0300

    add import

--- data/src/main/java/com/cheesecake/data/local/daos/TeamsDao.kt
+++ data/src/main/java/com/cheesecake/data/local/daos/TeamsDao.kt
@@ -3,6 +3,7 @@ package com.cheesecake.data.local.daos
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
+import com.cheesecake.data.local.models.LeagueLocalDto
 import com.cheesecake.data.local.models.TeamLocalDto
 import kotlinx.coroutines.flow.Flow
 
