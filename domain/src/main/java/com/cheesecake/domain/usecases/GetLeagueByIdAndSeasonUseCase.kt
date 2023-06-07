ff649b3ea2fea677d6542b53f1d61a6f30503117
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-07 16:13:50 +0300

    update use cases

--- domain/src/main/java/com/cheesecake/domain/usecases/GetLeagueByIdAndSeasonUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetLeagueByIdAndSeasonUseCase.kt
@@ -1,6 +1,6 @@
 package com.cheesecake.domain.usecases
 
-import com.cheesecake.domain.models.League
+import com.cheesecake.domain.entity.LeagueEntity
 import com.cheesecake.domain.repository.IFootballRepository
 import javax.inject.Inject
 
@@ -8,7 +8,7 @@ class GetLeagueByIdAndSeasonUseCase @Inject constructor(
     private val footballRepository: IFootballRepository,
 ) {
 
-    suspend operator fun invoke(leagueId: Int, leagueSeason: Int): League? {
+    suspend operator fun invoke(leagueId: Int, leagueSeason: Int): LeagueEntity? {
         return footballRepository.getLocallyLeagueByIdAndSeason(leagueId, leagueSeason)
             ?: footballRepository.getRemotelyLeagueByIdAndSeason(leagueId, leagueSeason)
     }
