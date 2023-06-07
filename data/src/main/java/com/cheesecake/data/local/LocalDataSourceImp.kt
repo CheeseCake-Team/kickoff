fbe8d94ade6826d4c60d73b8c0e5145c36f3e2bd
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-07 16:10:12 +0300

    implement getLocallyTeamsByIdAndSeason, updateOrInsertTeams, getFavouriteTeams, updateOrInsertTeam and deleteAllTeams

--- data/src/main/java/com/cheesecake/data/local/LocalDataSourceImp.kt
+++ data/src/main/java/com/cheesecake/data/local/LocalDataSourceImp.kt
@@ -2,45 +2,34 @@ package com.cheesecake.data.local
 
 import com.cheesecake.data.local.daos.LeagueDao
 import com.cheesecake.data.local.daos.TeamsDao
-import com.cheesecake.data.local.models.FavouriteTeamLocalDto
 import com.cheesecake.data.local.models.LeagueLocalDto
 import com.cheesecake.data.local.models.TeamLocalDto
 import com.cheesecake.data.repository.LocalDataSource
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class LocalDataSourceImp @Inject constructor(
     private val teamsDao: TeamsDao,
     private val leagueDao: LeagueDao,
+) : LocalDataSource {
 
-    ) : LocalDataSource {
-
-    override fun getAllTeams(): Flow<List<TeamLocalDto>> {
-        return teamsDao.getAllTeams()
+    override fun getLocallyTeamsByIdAndSeason(leagueId: Int, season: Int): List<TeamLocalDto> {
+        return teamsDao.getLocallyTeamsByIdAndSeason(leagueId, season)
     }
 
-    override suspend fun upsertAll(teams: List<TeamLocalDto>) {
-        teamsDao.upsertAll(teams)
+    override suspend fun updateOrInsertTeams(teams: List<TeamLocalDto>) {
+        teamsDao.updateOrInsertTeams(teams)
     }
 
-    override suspend fun insertTeam(favTeamDto: FavouriteTeamLocalDto) {
-        //favouriteTeamDao.insertTeam(favTeamDto)
+    override suspend fun getFavouriteTeams(): List<TeamLocalDto> {
+        return teamsDao.getFavouriteTeams()
     }
 
-    override suspend fun getFavTeams(): Flow<List<FavouriteTeamLocalDto>> {
-
-        //return favouriteTeamDao.getFavTeams()
-        TODO("NOT IMPLEMENTED YET")
+    override suspend fun updateOrInsertTeam(team: TeamLocalDto) {
+        teamsDao.updateOrInsertTeam(team)
     }
 
-
-    override fun getLeague(): Flow<List<LeagueLocalDto>> {
-        return leagueDao.getLeague()
-    }
-
-
-    override suspend fun deleteAll() {
-       teamsDao.deleteAll()
+    override suspend fun deleteAllTeams() {
+        teamsDao.deleteAllTeams()
     }
 
     override suspend fun getLeagueByIdAndSeason(leagueId: Int, leagueSeason: Int): LeagueLocalDto? {
