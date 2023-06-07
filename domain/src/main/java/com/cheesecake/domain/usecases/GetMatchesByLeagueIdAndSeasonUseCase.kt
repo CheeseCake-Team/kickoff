31376a21d0b5f38b72d0a978eaca88f29b0221f3
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-07 03:52:30 +0300

    create and implement GetMatchesByLeagueIdAndSeasonUseCase

---
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetMatchesByLeagueIdAndSeasonUseCase.kt
@@ -0,0 +1,15 @@
+package com.cheesecake.domain.usecases
+
+import com.cheesecake.domain.models.FixtureEntity
+import com.cheesecake.domain.repository.IFootballRepository
+import javax.inject.Inject
+
+class GetMatchesByLeagueIdAndSeasonUseCase @Inject constructor(
+    private val footballRepository: IFootballRepository,
+) {
+
+
+    suspend operator fun invoke(timeZone: String, leagueId: Int, Season: String): List<FixtureEntity> {
+        return footballRepository.getMatchesByLeagueIdAndSeason(timeZone, leagueId, Season)
+    }
+}
