5d9cdc81506bff74254c5e56e00e731f2709dfa6
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-07 16:01:30 +0300

    add mapToDomain functions

--- data/src/main/java/com/cheesecake/data/repository/models/response/FixtureResponse.kt
+++ data/src/main/java/com/cheesecake/data/repository/models/response/FixtureResponse.kt
@@ -6,6 +6,7 @@ import com.cheesecake.data.repository.models.dto.LeagueCountrySeasonRoundDTO
 import com.cheesecake.data.repository.models.dto.MatchesDTO
 import com.cheesecake.data.repository.models.dto.ScoreBlockDTO
 import com.cheesecake.data.repository.models.dto.TeamHomeAwayDTO
+import com.cheesecake.domain.entity.FixtureEntity
 import com.google.gson.annotations.SerializedName
 
 data class FixtureResponse(
@@ -20,3 +21,21 @@ data class FixtureResponse(
     @SerializedName("score")
     val score: ScoreBlockDTO
 )
+
+
+fun FixtureResponse.mapToDomain(): FixtureEntity {
+    return FixtureEntity(
+        homeTeamName = this.teams.home.name,
+        homeTeamLogo = this.league.logo,
+        homeTeamGoals = this.goals.home,
+        awayTeamName = this.teams.away.name,
+        awayTeamLogo = this.teams.away.logo,
+        awayTeamGoals = this.goals.away,
+        matchTime = this.fixtureDTO.timestamp,
+        isFinished = false
+    )
+}
+
+fun List<FixtureResponse>.mapToDomain(): List<FixtureEntity> {
+    return this.map { it.mapToDomain() }
+}
