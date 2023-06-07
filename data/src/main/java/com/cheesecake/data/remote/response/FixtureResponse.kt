5f772b8c14d68cc3a6e41c7e7cba1245df47aa75
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-07 03:47:31 +0300

    implement two mappers in FixtureResponse

--- data/src/main/java/com/cheesecake/data/remote/response/FixtureResponse.kt
+++ data/src/main/java/com/cheesecake/data/remote/response/FixtureResponse.kt
@@ -6,6 +6,7 @@ import com.cheesecake.data.models.base.LeagueCountrySeasonRound
 import com.cheesecake.data.models.base.Matches
 import com.cheesecake.data.models.base.ScoreBlock
 import com.cheesecake.data.models.base.TeamHomeAway
+import com.cheesecake.domain.models.FixtureEntity
 import com.google.gson.annotations.SerializedName
 
 data class FixtureResponse(
@@ -20,3 +21,20 @@ data class FixtureResponse(
     @SerializedName("score")
     val score: ScoreBlock
 )
+
+fun FixtureResponse.mapToDomain(): FixtureEntity {
+    return FixtureEntity(
+        homeTeamName = this.teams.home.name,
+        homeTeamLogo = this.league.logo,
+        homeTeamGoals = this.goals.home,
+        awayTeamName = this.teams.away.name,
+        awayTeamLogo = this.teams.away.logo,
+        awayTeamGoals = this.goals.away,
+        matchTime = fixture.timestamp,
+        isFinished = false
+    )
+}
+
+fun List<FixtureResponse>.mapToDomain(): List<FixtureEntity> {
+    return this.map { it.mapToDomain() }
+}
