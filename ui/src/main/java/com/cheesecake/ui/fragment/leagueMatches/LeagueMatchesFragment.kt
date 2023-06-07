24e1a584744daf0cc331a754abf0851df8b49a9b
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-07 16:18:24 +0300

    remove adapter and imports

--- ui/src/main/java/com/cheesecake/ui/fragment/leagueMatches/LeagueMatchesFragment.kt
+++ ui/src/main/java/com/cheesecake/ui/fragment/leagueMatches/LeagueMatchesFragment.kt
@@ -1,16 +1,11 @@
 package com.cheesecake.ui.fragment.leagueMatches
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.cheesecake.ui.R
 import com.cheesecake.ui.base.BaseFragment
 import com.cheesecake.ui.databinding.FragmentTeamsLeagueBinding
-import com.cheesecake.ui.fragment.leagueTeams.LeagueTeamsViewModel
-import com.cheesecake.ui.fragment.leagueTeams.adapter.TeamsAdapter
 
 class LeagueMatchesFragment : BaseFragment<FragmentTeamsLeagueBinding>() {
     override val layoutIdFragment: Int
@@ -19,7 +14,6 @@ class LeagueMatchesFragment : BaseFragment<FragmentTeamsLeagueBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.teamsRecyclerView.adapter = TeamsAdapter()
     }
 }
 
