df4836f1bd76ea74df1e267c2d9e3a9dca2a655b
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-07 16:12:03 +0300

    update updateOrInsertLeague, getRemotelyLeagueByIdAndSeason, getLocallyLeagueByIdAndSeason, getLeagueTopScorers, getLeagueStanding, getLeagueNameAndCountry

--- domain/src/main/java/com/cheesecake/domain/repository/IFootballRepository.kt
+++ domain/src/main/java/com/cheesecake/domain/repository/IFootballRepository.kt
@@ -1,26 +1,25 @@
 package com.cheesecake.domain.repository
 
-import com.cheesecake.domain.Entity.League
-import com.cheesecake.domain.Entity.Player
-import com.cheesecake.domain.Entity.Standings
-import com.cheesecake.domain.Entity.TeamInformation
+import com.cheesecake.domain.entity.PlayerEntity
+import com.cheesecake.domain.entity.StandingsEntity
+import com.cheesecake.domain.entity.FixtureEntity
+import com.cheesecake.domain.entity.LeagueEntity
+import com.cheesecake.domain.entity.TeamEntity
 
 interface IFootballRepository {
-    suspend fun getLeagueNameAndCountry(leagueId: Int, current: Boolean): List<League>
+    suspend fun getLeagueNameAndCountry(leagueId: Int, current: Boolean): List<LeagueEntity>
 
     suspend fun getLeagueCurrentRound(leagueId: Int, season: Int, current: Boolean): List<String>
 
-    suspend fun getNumberOfTeamsInLeague(leagueId: Int, season: Int): List<TeamInformation>
+    suspend fun getLeagueStanding(leagueId: Int, season: Int): List<StandingsEntity>
 
-    suspend fun getLeagueStanding(leagueId: Int, season: Int): List<Standings>
+    suspend fun getLeagueTopScorers(leagueId: Int, season: Int): List<PlayerEntity>
 
-    suspend fun getLeagueTopScorers(leagueId: Int, season: Int): List<Player>
+    suspend fun getLocallyLeagueByIdAndSeason(leagueId: Int, leagueSeason: Int): LeagueEntity?
 
-    suspend fun getLocallyLeagueByIdAndSeason(leagueId: Int, leagueSeason: Int): League?
+    suspend fun getRemotelyLeagueByIdAndSeason(leagueId: Int, leagueSeason: Int): LeagueEntity?
 
-    suspend fun getRemotelyLeagueByIdAndSeason(leagueId: Int, leagueSeason: Int): League?
-
-    suspend fun updateOrInsertLeague(league: League)
+    suspend fun updateOrInsertLeague(leagueEntity: LeagueEntity)
 
     suspend fun getMatchesByLeagueIdAndSeason(timeZone: String, leagueId: Int, Season: String): List<FixtureEntity>
 
