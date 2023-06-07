2a7c55928ebd56ffcfc236d6dd48dd35816a22a7
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-07 16:07:27 +0300

    implement GetAllTeamsInLeagueWithSeason

--- domain/src/main/java/com/cheesecake/domain/usecases/GetAllTeamsInLeagueWithSeason.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetAllTeamsInLeagueWithSeason.kt
@@ -1,17 +1,20 @@
 package com.cheesecake.domain.usecases
 
-//import com.cheesecake.data.repository.teams.TeamsRepository
-//import javax.inject.Inject
-//
-//class GetAllTeamsInLeagueWithSeason @Inject constructor(
-//    private val teamsRepository: TeamsRepository,
-//) {
-//
-////    suspend operator fun invoke(leagueId: Int, season: Int) : Flow<List<Team>> {
-////        return teamsRepository.getTeamsByLeagueAndSeason(leagueId, season).map { localTeamsDto ->
-////            localTeamsDto.map { it.toEntity() }
-////        }
-////    }
-//
-//
-//}
+import com.cheesecake.domain.entity.TeamEntity
+import com.cheesecake.domain.repository.IFootballRepository
+import javax.inject.Inject
+
+class GetAllTeamsInLeagueWithSeason @Inject constructor(
+    private val footballRepository: IFootballRepository,
+) {
+
+    suspend operator fun invoke(leagueId: Int, leagueSeason: Int): List<TeamEntity> {
+        val localTeams = footballRepository.getLocallyTeamsByIdAndSeason(leagueId, leagueSeason)
+        if (localTeams.isEmpty()) {
+            val remoteTeams = footballRepository.getRemotelyTeamsByIdAndSeason(leagueId, leagueSeason)
+            footballRepository.updateOrInsertTeams(remoteTeams, leagueId, leagueSeason)
+        }
+        return footballRepository.getLocallyTeamsByIdAndSeason(leagueId, leagueSeason)
+    }
+
+}
