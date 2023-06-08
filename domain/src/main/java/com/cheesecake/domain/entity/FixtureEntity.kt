768f0ecf185b1996d0215b9311dc64660a49e9f3
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-08 14:27:48 +0300

    update FixtureEntity

--- domain/src/main/java/com/cheesecake/domain/entity/FixtureEntity.kt
+++ domain/src/main/java/com/cheesecake/domain/entity/FixtureEntity.kt
@@ -3,10 +3,11 @@ package com.cheesecake.domain.entity
 data class FixtureEntity(
     val homeTeamName: String,
     val homeTeamLogo: String,
-    val homeTeamGoals: Int?,
+    val homeTeamGoals: String?,
     val awayTeamName: String,
     val awayTeamLogo: String,
-    val awayTeamGoals: Int?,
+    val awayTeamGoals: String?,
     val matchTime: Int?,
+    val matchDate: String,
     val isFinished: Boolean = false
 )
