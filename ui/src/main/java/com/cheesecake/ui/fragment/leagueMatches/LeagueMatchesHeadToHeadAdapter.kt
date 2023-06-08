7d36df031c1beb8c52d14d131c42efcc85d8030f
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-08 14:46:58 +0300

    implement LeagueMatchesHeadToHeadAdapter

---
+++ ui/src/main/java/com/cheesecake/ui/fragment/leagueMatches/LeagueMatchesHeadToHeadAdapter.kt
@@ -0,0 +1,9 @@
+package com.cheesecake.ui.fragment.leagueMatches
+
+import com.cheesecake.domain.entity.FixtureEntity
+import com.cheesecake.ui.R
+import com.cheesecake.ui.base.BaseAdapter
+
+class LeagueMatchesHeadToHeadAdapter() : BaseAdapter<FixtureEntity>(null) {
+    override val layoutId = R.layout.item_head_to_head
+}
