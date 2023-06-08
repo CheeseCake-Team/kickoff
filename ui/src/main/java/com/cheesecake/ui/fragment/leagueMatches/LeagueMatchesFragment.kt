6434016ae6393283804f2d54c75b1b359cb14faf
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-08 17:31:39 +0300

    add adapter add hilt annotation

--- ui/src/main/java/com/cheesecake/ui/fragment/leagueMatches/LeagueMatchesFragment.kt
+++ ui/src/main/java/com/cheesecake/ui/fragment/leagueMatches/LeagueMatchesFragment.kt
@@ -5,14 +5,17 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.cheesecake.ui.R
 import com.cheesecake.ui.base.BaseFragment
-import com.cheesecake.ui.databinding.FragmentTeamsLeagueBinding
+import com.cheesecake.ui.databinding.FragmentLeagueMatchesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class LeagueMatchesFragment : BaseFragment<FragmentTeamsLeagueBinding>() {
+@AndroidEntryPoint
+class LeagueMatchesFragment : BaseFragment<FragmentLeagueMatchesBinding>() {
     override val layoutIdFragment = R.layout.fragment_league_matches
     override val viewModel: LeagueMatchesViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.matchesRecyclerView.adapter = LeagueMatchesDateMatchAdapter()
     }
 }
 
