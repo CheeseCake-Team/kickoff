670a806cbe1d60cf58cf55b6b0e68b8865f6c470
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-13 21:39:38 +0300

    fix FavoriteLeaguesAdapter

--- presentation/src/main/java/com/cheesecake/presentation/ui/favoriteLeagues/FavoriteLeaguesAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/ui/favoriteLeagues/FavoriteLeaguesAdapter.kt
@@ -5,5 +5,5 @@ import com.cheesecake.presentation.R
 import com.cheesecake.presentation.base.BaseAdapter
 
 class FavoriteLeaguesAdapter: BaseAdapter<League>(null) {
-    override val layoutId = R.layout.item_favorite_team
+    override val layoutId = R.layout.item_favorite_leagues
 }
