9d1648db3f3794c45693afbea0b0d81bf7f96ea8
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-13 20:55:26 +0300

    implement FavoriteTeamsViewModel

--- presentation/src/main/java/com/cheesecake/presentation/ui/favoriteTeams/FavoriteTeamsViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/ui/favoriteTeams/FavoriteTeamsViewModel.kt
@@ -1,7 +1,38 @@
 package com.cheesecake.presentation.ui.favoriteTeams
 
-import androidx.lifecycle.ViewModel
 
-class FavoriteTeamsViewModel: ViewModel() {
+import androidx.lifecycle.viewModelScope
+import com.cheesecake.domain.entity.Team
+import com.cheesecake.domain.usecases.GetFavoriteTeamsUseCase
+import com.cheesecake.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class FavoriteTeamsViewModel(
+    private val getFavoriteTeamsUseCase: GetFavoriteTeamsUseCase
+) : BaseViewModel<FavoriteTeamUIState>(FavoriteTeamUIState()) {
+
+    init {
+        getFavoriteTeams()
+    }
+
+    private fun getFavoriteTeams() {
+        tryToExecute({ getFavoriteTeamsUseCase() }, ::onSuccess, ::onError)
+    }
+
+    private fun onSuccess(flow: Flow<List<Team>>) {
+        viewModelScope.launch {
+            flow.collect { teams ->
+                _state.update { it.copy(teams = teams, isTeamsIsEmpty = teams.isEmpty(),isLoading = false) }
+            }
+        }
+    }
+
+    private fun onError(e: Throwable) {
+        _state.update { it.copy(errorMessage = e.localizedMessage ?: "Unknown error.", isLoading = false) }
+    }
 
 }
