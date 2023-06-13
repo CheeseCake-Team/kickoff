1756cbd390fc974d840971f6a04f82d61b82c26a
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-13 20:10:04 +0300

    update getFavouritePlayers method remove getLocallyPlayer

--- data/src/main/java/com/cheesecake/data/local/daos/PlayerDao.kt
+++ data/src/main/java/com/cheesecake/data/local/daos/PlayerDao.kt
@@ -4,12 +4,10 @@ import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
 import com.cheesecake.data.local.models.PlayerLocalDto
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlayerDao {
-
-    @Query("SELECT * FROM player_table WHERE playerId = :playerId")
-    fun getLocallyPlayer(playerId: Int): List<PlayerLocalDto>
 
     @Upsert
     suspend fun updateOrInsertPlayers(players: List<PlayerLocalDto>)
@@ -21,7 +19,7 @@ interface PlayerDao {
     suspend fun deleteAllPlayers()
 
     @Query("SELECT * FROM player_table WHERE isFavourite = 1")
-    suspend fun getFavouritePlayers(): List<PlayerLocalDto>
+    suspend fun getFavouritePlayers(): Flow<List<PlayerLocalDto>>
 
     @Query("DELETE FROM player_table WHERE playerId = :playerId")
     suspend fun deletePlayerById(playerId: Int)
