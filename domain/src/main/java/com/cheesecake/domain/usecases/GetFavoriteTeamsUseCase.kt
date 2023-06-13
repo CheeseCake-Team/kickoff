26f7aca7ee0937bf7261681dc60a5ec1a700966c
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-13 21:38:42 +0300

    fix GetFavoriteTeamsUseCase

--- domain/src/main/java/com/cheesecake/domain/usecases/GetFavoriteTeamsUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetFavoriteTeamsUseCase.kt
@@ -3,8 +3,9 @@ package com.cheesecake.domain.usecases
 import com.cheesecake.domain.entity.Team
 import com.cheesecake.domain.repository.IFootballRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class GetFavoriteTeamsUseCase(
+class GetFavoriteTeamsUseCase @Inject constructor(
     private val footballRepository: IFootballRepository,
 ) {
 
