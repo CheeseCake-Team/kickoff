e277a91e823abe1f60df28f33f1457bbd7338c85
yaseen <yz456359@gmail.com>
2023-06-14 19:03:08 +0300

    design standings fragment without code

---
+++ presentation/src/main/java/com/cheesecake/presentation/ui/standings/StandingsFragment.kt
@@ -0,0 +1,15 @@
+package com.cheesecake.presentation.ui.standings
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import com.cheesecake.presentation.R
+import com.cheesecake.presentation.base.BaseFragment
+import com.cheesecake.presentation.databinding.FragmentStandingsBinding
+
+
+class StandingsFragment : BaseFragment<FragmentStandingsBinding>() {
+    override val layoutIdFragment = R.layout.fragment_standings
+    override val viewModel: ViewModel
+        get() = TODO("Not yet implemented")
+
+}
