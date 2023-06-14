5b772a71af50e577120b8e7237267a6c015dae8d
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-14 23:26:06 +0300

    implement SettingsFragment

---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/Settings/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.cheesecake.presentation.screens.Settings
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
