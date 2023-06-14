764b93ffcbeef1a093ec48c54ac7adde051918d7
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 01:09:22 +0300

    update database

--- data/src/main/java/com/cheesecake/data/local/database/KickoffDatabase.kt
+++ data/src/main/java/com/cheesecake/data/local/database/KickoffDatabase.kt
@@ -3,14 +3,12 @@ package com.cheesecake.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.cheesecake.data.local.daos.LeagueDao
-import com.cheesecake.data.local.daos.PlayerDao
 import com.cheesecake.data.local.daos.TeamsDao
 import com.cheesecake.data.local.models.LeagueLocalDTO
-import com.cheesecake.data.local.models.PlayerLocalDto
 import com.cheesecake.data.local.models.TeamLocalDTO
 
 @Database(
-    entities = [TeamLocalDTO::class, LeagueLocalDTO::class, PlayerLocalDto::class],
+    entities = [TeamLocalDTO::class, LeagueLocalDTO::class],
     version = 1
 ) //TODO add all entities into the annotation
 
@@ -19,7 +17,5 @@ abstract class KickoffDatabase : RoomDatabase() {
     abstract fun getTeamsDao(): TeamsDao
 
     abstract fun getLeagueDao(): LeagueDao
-
-    abstract fun getPlayerDao(): PlayerDao
 
 }
