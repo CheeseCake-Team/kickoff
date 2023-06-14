24ef9314f01669758b98f60d8f0b17c807b4182e
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 01:10:25 +0300

    update LeagueMatchesHeadToHeadAdapter.kt

--- presentation/src/main/java/com/cheesecake/presentation/ui/leagueMatches/LeagueMatchesHeadToHeadAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/ui/leagueMatches/LeagueMatchesHeadToHeadAdapter.kt
@@ -1,9 +1,22 @@
 package com.cheesecake.presentation.ui.leagueMatches
 
+import androidx.recyclerview.widget.DiffUtil
 import com.cheesecake.domain.entity.Fixture
 import com.cheesecake.presentation.R
-import com.cheesecake.presentation.base.BaseAdapter
+import com.cheesecake.presentation.base.BaseListAdapter
 
-class LeagueMatchesHeadToHeadAdapter : BaseAdapter<Fixture>(null) {
-    override val layoutId = R.layout.item_head_to_head
+class LeagueMatchesHeadToHeadAdapter :
+    BaseListAdapter<Fixture>(FixtureDiffUtils(), null) {
+    override val layoutId: Int
+        get() = R.layout.item_head_to_head
+
+}
+
+class FixtureDiffUtils : DiffUtil.ItemCallback<Fixture>() {
+    override fun areItemsTheSame(oldItem: Fixture, newItem: Fixture): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: Fixture, newItem: Fixture): Boolean =
+        oldItem == newItem
+
 }
