469c9e423a7ae6549608267d7ae618c1e65440fd
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 01:08:36 +0300

    update FavoriteLeaguesAdapter.kt

--- presentation/src/main/java/com/cheesecake/presentation/ui/favoriteLeagues/FavoriteLeaguesAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/ui/favoriteLeagues/FavoriteLeaguesAdapter.kt
@@ -1,9 +1,18 @@
 package com.cheesecake.presentation.ui.favoriteLeagues
 
-import com.cheesecake.domain.entity.League
+import androidx.recyclerview.widget.DiffUtil
 import com.cheesecake.presentation.R
-import com.cheesecake.presentation.base.BaseAdapter
+import com.cheesecake.presentation.base.BaseListAdapter
 
-class FavoriteLeaguesAdapter: BaseAdapter<League>(null) {
+class FavoriteLeaguesAdapter: BaseListAdapter<FavoriteLeagueUIState>(LeagueUIStateDiffUtil(), null) {
     override val layoutId = R.layout.item_favorite_leagues
+}
+
+class LeagueUIStateDiffUtil: DiffUtil.ItemCallback<FavoriteLeagueUIState>() {
+    override fun areItemsTheSame(oldItem: FavoriteLeagueUIState, newItem: FavoriteLeagueUIState): Boolean =
+        oldItem.id == newItem.id
+
+    override fun areContentsTheSame(oldItem: FavoriteLeagueUIState, newItem: FavoriteLeagueUIState): Boolean =
+        oldItem == newItem
+
 }
