f605cec0ba9c353103432d3aba6982cbd2e1d561
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-16 00:59:06 +0300

    implement SettingsViewModel

--- presentation/src/main/java/com/cheesecake/presentation/screens/Settings/SettingsFragment.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/Settings/SettingsFragment.kt
@@ -2,9 +2,14 @@ package com.cheesecake.presentation.screens.Settings
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.cheesecake.presentation.R
+import com.cheesecake.presentation.base.BaseFragment
+import com.cheesecake.presentation.databinding.FragmentSettingsBinding
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
+    override val layoutIdFragment = R.layout.fragment_settings
+    override val viewModel: SettingsViewModel by viewModels()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/Settings/SettingsViewModel.kt
@@ -0,0 +1,14 @@
+package com.cheesecake.presentation.screens.Settings
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+}
