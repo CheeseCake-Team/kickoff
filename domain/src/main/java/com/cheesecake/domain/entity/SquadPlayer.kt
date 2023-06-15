ad9a324c232a6b843ab13548bd075fa1294a4202
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 23:38:03 +0300

    create SquadPlayer

---
+++ domain/src/main/java/com/cheesecake/domain/entity/SquadPlayer.kt
@@ -0,0 +1,10 @@
+package com.cheesecake.domain.entity
+
+data class SquadPlayer(
+    val id: Int,
+    val name: String,
+    val age: Int,
+    val number: Int,
+    val position: String,
+    val photo: String
+)
