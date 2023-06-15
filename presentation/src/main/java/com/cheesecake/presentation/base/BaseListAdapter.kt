fffb4d938518149298300dc8ce2f6ebc16d4f9bf
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 15:32:58 +0300

    update BaseListAdapter

--- presentation/src/main/java/com/cheesecake/presentation/base/BaseListAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/base/BaseListAdapter.kt
@@ -12,7 +12,6 @@ import com.cheesecake.presentation.screens.leagueMatches.LeagueMatchesHeadToHead
 
 abstract class BaseListAdapter<T>(
     diffutils: DiffUtil.ItemCallback<T>,
-    private val listener: BaseInteractionListener?
 ) : ListAdapter<T, BaseListAdapter.BaseViewHolder<T>>(diffutils) {
 
     abstract val layoutId: Int
@@ -27,14 +26,13 @@ abstract class BaseListAdapter<T>(
 
     override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
         val currentItem = getItem(position)
-        holder.bind(currentItem, listener)
+        holder.bind(currentItem)
     }
 
     open class BaseViewHolder<T>(private val binding: ViewDataBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(currentItem: T, listener: BaseInteractionListener?) {
+        fun bind(currentItem: T) {
             binding.setVariable(androidx.databinding.library.baseAdapters.BR.item, currentItem)
-            binding.setVariable(androidx.databinding.library.baseAdapters.BR.listener, listener)
 
             when(binding) {
                 is ItemDateMatchesBinding -> {
