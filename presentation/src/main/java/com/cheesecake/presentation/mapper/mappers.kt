8e820686f5a85400b5748b2079704febc1007034
Ziad mahmoud <ziaadmahmoud2001@hotmail.com>
2023-06-15 22:57:19 +0300

    to convert teamcountry model to ui state model

--- presentation/src/main/java/com/cheesecake/presentation/mapper/mappers.kt
+++ presentation/src/main/java/com/cheesecake/presentation/mapper/mappers.kt
@@ -1,17 +1,27 @@
 package com.cheesecake.presentation.mapper
 
 import com.cheesecake.domain.entity.Team
+import com.cheesecake.domain.entity.TeamCountry
+import com.cheesecake.presentation.models.TeamCountryUIState
 import com.cheesecake.presentation.models.TeamUIState
 
 fun Team.toUIModel(): TeamUIState {
     return TeamUIState(
         teamId = this.id,
-        teamName= this.name,
-        founded= this.yearFounded,
-        teamCountry= this.country,
-        venueCapacity= this.stadiumCapacity,
-        venueName= this.stadiumName,
-        imageUrl= this.imageUrl,
-        isFavourite= this.isFavourite,
+        teamName = this.name,
+        founded = this.yearFounded,
+        teamCountry = this.country,
+        venueCapacity = this.stadiumCapacity,
+        venueName = this.stadiumName,
+        imageUrl = this.imageUrl,
+        isFavourite = this.isFavourite,
+    )
+}
+
+
+fun TeamCountry.toUIModel(): TeamCountryUIState {
+    return TeamCountryUIState(
+        name = name,
+        flag = flag
     )
 }
