3236ae12a0297cfe4eb8fc4f61bf1ef94a7a8c7b
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 23:43:30 +0300

    update GetTeamSquadByIdUseCase

--- domain/src/main/java/com/cheesecake/domain/usecases/GetTeamSquadByIdUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetTeamSquadByIdUseCase.kt
@@ -1,13 +1,14 @@
 package com.cheesecake.domain.usecases
 
-import com.cheesecake.domain.entity.PlayerStatistics
+import com.cheesecake.domain.entity.SquadPlayer
 import com.cheesecake.domain.repository.IFootballRepository
 import javax.inject.Inject
 
 class GetTeamSquadByIdUseCase@Inject constructor(
     private val footballRepository: IFootballRepository) {
 
-    suspend operator fun invoke(teamId: Int): List<Pair<String, List<PlayerStatistics>>>{
+    suspend operator fun invoke(teamId: Int): List<Pair<String, List<SquadPlayer>>>{
         return footballRepository.getSquadOfTeam(teamId).groupBy { it.position }.toList()
     }
+
 }
