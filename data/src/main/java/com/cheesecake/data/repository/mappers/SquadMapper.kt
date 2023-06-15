a8f963526b69c7bf81aaba2b4acf0be619a4ddd8
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 23:39:36 +0300

    create SquadMapper.kt

---
+++ data/src/main/java/com/cheesecake/data/repository/mappers/SquadMapper.kt
@@ -0,0 +1,10 @@
+package com.cheesecake.data.repository.mappers
+
+import com.cheesecake.data.remote.models.SquadDTO
+import com.cheesecake.domain.entity.SquadPlayer
+
+@JvmName("squadDTOsToSquadPlayers")
+fun List<SquadDTO>.toEntity(): List<SquadPlayer> = this.flatMap{ it.players.map { player -> player.toEntity() } }
+
+@JvmName("squadDTOToSquadPlayer")
+fun SquadDTO.Player.toEntity(): SquadPlayer = SquadPlayer(id, name, age, number, position, photo)
