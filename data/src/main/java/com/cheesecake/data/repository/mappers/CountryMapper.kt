7d414c68b2888aedd7486e6513b1380b9f4e2e2b
Ziad mahmoud <ziaadmahmoud2001@hotmail.com>
2023-06-15 22:24:26 +0300

    create country mapper

--- data/src/main/java/com/cheesecake/data/repository/mappers/CountryMapper.kt
+++ data/src/main/java/com/cheesecake/data/repository/mappers/CountryMapper.kt
@@ -1,12 +1,34 @@
 package com.cheesecake.data.repository.mappers
 
-import com.cheesecake.data.local.models.CountryLocalDto
-import com.cheesecake.domain.entity.Country
+import com.cheesecake.data.local.models.TeamCountriesLocalDTO
+import com.cheesecake.data.remote.models.TeamCountriesDTO
+import com.cheesecake.domain.entity.TeamCountry
 
-@JvmName("CountryToCountryLocalDTO")
-fun Country.toLocal(): CountryLocalDto = CountryLocalDto(
-    countryId = countryId,
-    name = name ,
+@JvmName("teamCountryDTOToTeamCountry")
+fun TeamCountriesDTO.toEntity(): TeamCountry =
+    TeamCountry(
+        name = name,
+        flag = flag ?: ""
+    )
+
+fun TeamCountriesLocalDTO.toEntity(): TeamCountry =
+    TeamCountry(
+        name = name,
+        flag = flagImageUrl
+    )
+
+@JvmName("teamCountryToTeamCountryLocalDTO")
+fun TeamCountry.toLocal(): TeamCountriesLocalDTO = TeamCountriesLocalDTO(
+    name = name,
     flagImageUrl = flag
 )
+
+@JvmName("listOfTeamCountryDTOToListOfTeamCountryLocalDTO")
+fun List<TeamCountry>.toLocal(): List<TeamCountriesLocalDTO> = map { it.toLocal() }
+
+@JvmName("listOfTeamCountryDTOToListOfTeamCountry")
+fun List<TeamCountriesDTO>.toEntity(): List<TeamCountry> = map { it.toEntity() }
+
+@JvmName("listOfTeamCountryLocalDTOToListOfTeamCountry")
+fun List<TeamCountriesLocalDTO>.toEntity(): List<TeamCountry> = map { it.toEntity() }
 
