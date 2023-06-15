6309fb44657fef1ca540ddcd4eba2b5c39ac28bf
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 23:42:18 +0300

    update adapter

--- presentation/src/main/java/com/cheesecake/presentation/screens/teamPlayers/TeamPLayersPositionsAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/teamPlayers/TeamPLayersPositionsAdapter.kt
@@ -2,26 +2,26 @@ package com.cheesecake.presentation.screens.teamPlayers
 
 
 import androidx.recyclerview.widget.DiffUtil
-import com.cheesecake.domain.entity.PlayerStatistics
+import com.cheesecake.domain.entity.SquadPlayer
 import com.cheesecake.presentation.R
 import com.cheesecake.presentation.base.BaseListAdapter
 
-class TeamPLayersPositionsAdapter : BaseListAdapter<Pair<String,List<PlayerStatistics>>>(
+class TeamPLayersPositionsAdapter : BaseListAdapter<Pair<String,List<SquadPlayer>>>(
     DateTeamPlayersPositionDiffUtil(),
     null
 ){
     override val layoutId = R.layout.item_team_players_positions
 }
-class DateTeamPlayersPositionDiffUtil() : DiffUtil.ItemCallback<Pair<String, List<PlayerStatistics>>>() {
+class DateTeamPlayersPositionDiffUtil() : DiffUtil.ItemCallback<Pair<String, List<SquadPlayer>>>() {
 
     override fun areItemsTheSame(
-        oldItem: Pair<String, List<PlayerStatistics>>,
-        newItem: Pair<String, List<PlayerStatistics>>
+        oldItem: Pair<String, List<SquadPlayer>>,
+        newItem: Pair<String, List<SquadPlayer>>
     ): Boolean = oldItem.first == newItem.first
 
     override fun areContentsTheSame(
-        oldItem: Pair<String, List<PlayerStatistics>>,
-        newItem: Pair<String, List<PlayerStatistics>>
+        oldItem: Pair<String, List<SquadPlayer>>,
+        newItem: Pair<String, List<SquadPlayer>>
     ): Boolean = oldItem.second == newItem.second
 
 }
--- presentation/src/main/java/com/cheesecake/presentation/screens/teamPlayers/TeamPlayerAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/teamPlayers/TeamPlayerAdapter.kt
@@ -1,22 +1,22 @@
 package com.cheesecake.presentation.screens.teamPlayers
 
 import androidx.recyclerview.widget.DiffUtil
-import com.cheesecake.domain.entity.PlayerStatistics
+import com.cheesecake.domain.entity.SquadPlayer
 import com.cheesecake.presentation.R
 import com.cheesecake.presentation.base.BaseListAdapter
 
-class TeamPlayerAdapter: BaseListAdapter<PlayerStatistics>(
+class TeamPlayerAdapter: BaseListAdapter<SquadPlayer>(
     PlayerDiffUtils(),
     null
 ) {
     override val layoutId = R.layout.item_team_player
 }
 
-class PlayerDiffUtils : DiffUtil.ItemCallback<PlayerStatistics>() {
-    override fun areItemsTheSame(oldItem: PlayerStatistics, newItem: PlayerStatistics): Boolean =
+class PlayerDiffUtils : DiffUtil.ItemCallback<SquadPlayer>() {
+    override fun areItemsTheSame(oldItem: SquadPlayer, newItem: SquadPlayer): Boolean =
         oldItem.id == newItem.id
 
-    override fun areContentsTheSame(oldItem: PlayerStatistics, newItem: PlayerStatistics): Boolean =
+    override fun areContentsTheSame(oldItem: SquadPlayer, newItem: SquadPlayer): Boolean =
         oldItem == newItem
 
 }
