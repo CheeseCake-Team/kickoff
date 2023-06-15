6455109f454e45119308cf8828f14d2fd74445a2
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-15 20:24:54 +0300

    create FavoriteTeamMapper.kt

---
+++ presentation/src/main/java/com/cheesecake/presentation/mapper/FavoriteTeamMapper.kt
@@ -0,0 +1,20 @@
+package com.cheesecake.presentation.mapper
+
+import com.cheesecake.domain.entity.Team
+import com.cheesecake.presentation.screens.favoriteTeams.FavoriteTeamItemUiState
+
+
+@JvmName("teamToFavoriteTeamItemUiState")
+fun Team.toUIState(
+    onFavoriteButtonClick: (Int) -> Unit,
+    onFavoriteLeagueClick: (Int) -> Unit
+): FavoriteTeamItemUiState =
+    FavoriteTeamItemUiState(
+        teamId = this.id,
+        name = this.name,
+        country = this.country,
+        imageUrl = this.imageUrl,
+        isFavorite = this.isFavourite,
+        onFavoriteClick = onFavoriteButtonClick,
+        onFavoriteTeamClick = onFavoriteLeagueClick,
+    )
