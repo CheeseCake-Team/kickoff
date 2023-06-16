46124d18ca3bfaaa8e3eddb7f5b7e0ffeef73ccd
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-16 20:24:01 +0300

    make toggleFavourite private update onSuccess method

--- presentation/src/main/java/com/cheesecake/presentation/screens/league/LeagueViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/league/LeagueViewModel.kt
@@ -5,7 +5,6 @@ import com.cheesecake.domain.entity.League
 import com.cheesecake.domain.usecases.FavouriteLeagueUseCase
 import com.cheesecake.domain.usecases.GetLeagueByIdAndSeasonUseCase
 import com.cheesecake.presentation.base.BaseViewModel
-import com.cheesecake.presentation.mapper.toUIState
 import com.cheesecake.presentation.models.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.update
@@ -16,13 +15,13 @@ import javax.inject.Inject
 class LeagueViewModel @Inject constructor(
     private val getLeagueByIdAndSeasonUseCase: GetLeagueByIdAndSeasonUseCase,
     private val favouriteLeagueUseCase: FavouriteLeagueUseCase
-) : BaseViewModel<LeagueUIState, LeagueNavigationEvent?>(LeagueUIState(), Event()) {
+) : BaseViewModel<LeagueUIState, LeagueNavigationEvent>(LeagueUIState(), Event()) {
 
     init {
         getLeague(39, 2022)
     }
 
-    fun toggleFavourite(leagueId: Int, leagueSeason: Int) {
+    private fun toggleFavourite(leagueId: Int, leagueSeason: Int) {
         viewModelScope.launch {
             favouriteLeagueUseCase(leagueId, leagueSeason).let {
                 _state.update { uiState -> uiState.copy(isFavourite = it.isFavourite) }
@@ -38,15 +37,24 @@ class LeagueViewModel @Inject constructor(
         )
     }
 
-    private fun onSuccess(league: League?) {
-        league?.let {
-            _state.update { uiState ->
-                uiState.copy {
-                    league.toUIState(
-                        { toggleFavourite(it.leagueId, it.season.toInt()) },
-                        ::onBackClick
-                    )
-                }
+    private fun onSuccess(league: League) {
+        league.let {
+            _state.update { leagueUiState ->
+                leagueUiState.copy(
+                    leagueId = it.leagueId,
+                    leagueSeason = it.season.toInt(),
+                    leagueName = it.name,
+                    seasonStartEndYear = "${it.seasonStartYear}/${it.seasonEndYear}",
+                    imageUrl = it.imageUrl,
+                    isFavourite = it.isFavourite,
+                    onLeagueFavoriteClick = { leagueId, leagueSeason ->
+                        toggleFavourite(
+                            leagueId,
+                            leagueSeason
+                        )
+                    },
+                    onBackClick = { onBackClick() }
+                )
             }
         }
     }
