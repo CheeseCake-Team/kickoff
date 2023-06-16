4fd7301844aef956b42d29aa6d8b6abd31b33c7b
Anwaar Raafat <raafatanwaar@gmail.com>
2023-06-16 17:59:29 +0300

    edit team statistics ui state

--- presentation/src/main/java/com/cheesecake/presentation/screens/teamstatistics/TeamStatisticsUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/teamstatistics/TeamStatisticsUIState.kt
@@ -3,9 +3,30 @@ package com.cheesecake.presentation.screens.teamstatistics
 import com.cheesecake.domain.entity.Team
 
 data class TeamStatisticsUIState(
-    val home: Int,
-    val away: Int,
-    val total: Int,
+    val homePlayed: Int,
+    val homeWins: Int,
+    val homeLoses: Int,
+    val homeCleanSheet: Int,
+    val homeFailed: Int,
+    val homeGoalsFor: Int,
+    val homeGoalsAgainst: Int,
+
+    val awayPlayed: Int,
+    val awayWins: Int,
+    val awayLoses: Int,
+    val awayCleanSheet: Int,
+    val awayFailed: Int,
+    val awayGoalsFor: Int,
+    val awayGoalsAgainst: Int,
+
+    val totalPlayed: Int,
+    val totalWins: Int,
+    val totalLoses: Int,
+    val totalCleanSheet: Int,
+    val totalFailed: Int,
+    val totalGoalsFor: Int,
+    val totalGoalsAgainst: Int,
+
     val isLoading: Boolean = true,
     val data: List<Team> = emptyList(),
     val isError: String = "Error"
