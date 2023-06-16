a6389fa9d0428d0e5c16ecb5718597b12176b40b
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-16 22:12:21 +0300

    create HomeFragment

---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.cheesecake.presentation.screens.home
+
+import androidx.fragment.app.viewModels
+import com.cheesecake.presentation.R
+import com.cheesecake.presentation.base.BaseFragment
+import com.cheesecake.presentation.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override val layoutIdFragment = R.layout.fragment_home
+    override val viewModel: HomeViewModel by viewModels()
+
+}
