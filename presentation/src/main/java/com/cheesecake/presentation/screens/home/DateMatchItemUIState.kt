65e96c99799f226aedd8baf00a47594dd00d91e3
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-17 00:52:10 +0300

    update DateMatchItemUIState

--- presentation/src/main/java/com/cheesecake/presentation/screens/home/DateMatchItemUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/home/DateMatchItemUIState.kt
@@ -1,13 +1,16 @@
 package com.cheesecake.presentation.screens.home
 
 data class DateMatchItemUIState(
-    val leagueId: Int,
-    val leagueSeason: Int,
-    val timeZone: String,
-    val matchState: String,
-    val matchTime: String,
-    val homeTeamName: String,
-    val awayTeamName: String,
-    val homeTeamGoals: Int,
-    val awayTeamGoals: Int
+    val leagueId: Int = 0,
+    val leagueSeason: Int = 0,
+    val timeZone: String = "",
+    val matchState: String = "",
+    val matchTime: String = "",
+    val homeTeamName: String = "",
+    val awayTeamName: String = "",
+    val homeTeamGoals: Int = 0,
+    val awayTeamGoals: Int = 0,
+    val homeTeamImageUrl: String = "",
+    val awayTeamImageUrl: String = "",
+
 )
