e19392f051e65a69acf687fe94ce6a814a86f8c8
Anwaar Raafat <raafatanwaar@gmail.com>
2023-06-16 17:22:33 +0300

    fix team statistics

--- domain/src/main/java/com/cheesecake/domain/entity/TeamStatistics.kt
+++ domain/src/main/java/com/cheesecake/domain/entity/TeamStatistics.kt
@@ -1,18 +1,18 @@
 package com.cheesecake.domain.entity
 
 data class TeamStatisticsEntity(
-    val home: TeamStats,
-    val away: TeamStats,
-    val total: TeamStats
+    val played: ScoreStats,
+    val wins: ScoreStats,
+    val draws: ScoreStats,
+    val loses: ScoreStats,
+    val cleanSheet: ScoreStats,
+    val failedToScore: ScoreStats,
+    val goalsFor: ScoreStats,
+    val goalsAgainst: ScoreStats
 )
 
-data class TeamStats(
-    val played: Int,
-    val wins: Int,
-    val draws: Int,
-    val loses: Int,
-    val cleanSheet: Int,
-    val failedToScore: Int,
-    val goalsFor: Int,
-    val goalsAgainst: Int
+data class ScoreStats(
+    val home: Int,
+    val away: Int,
+    val total: Int
 )
--- presentation/src/main/java/com/cheesecake/presentation/screens/teamstatistics/TeamStatisticsUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/teamstatistics/TeamStatisticsUIState.kt
@@ -3,6 +3,9 @@ package com.cheesecake.presentation.screens.teamstatistics
 import com.cheesecake.domain.entity.Team
 
 data class TeamStatisticsUIState(
+    val home: Int,
+    val away: Int,
+    val total: Int,
     val isLoading: Boolean = true,
     val data: List<Team> = emptyList(),
     val isError: String = "Error"
