dc9d8c2dd27e82c64dbbb161de82150ed53cedb1
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-17 03:55:41 +0300

    implement getMatch() in MatchViewModel

--- presentation/src/main/java/com/cheesecake/presentation/screens/match/MatchViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/match/MatchViewModel.kt
@@ -1,18 +1,42 @@
-package com.cheesecake.presentation.ui.match
+package com.cheesecake.presentation.screens.match
 
-import com.cheesecake.domain.usecases.FavouriteLeagueUseCase
-import com.cheesecake.domain.usecases.GetLeagueByIdAndSeasonUseCase
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.cheesecake.domain.usecases.GetMatchDetailsUseCase
 import com.cheesecake.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MatchViewModel
 @Inject constructor(
-    private val getLeagueByIdAndSeasonUseCase: GetLeagueByIdAndSeasonUseCase,
-) :BaseViewModel<MatchUIState>(MatchUIState()) {
+    private val GetMatchDetailsUseCase: GetMatchDetailsUseCase,
+) : BaseViewModel<MatchUIState>(MatchUIState()) {
 
     init {
-
+        getMatch()
     }
+
+
+    private fun getMatch() {
+        viewModelScope.launch {
+            GetMatchDetailsUseCase("33-34", 2022, "Africa/Cairo").let { match ->
+                Log.i("Mujtaba", "getMatch: ${match}")
+                _state.update { uiState ->
+                    uiState.copy(
+                        homeTeamName = match.homeTeamName,
+                        homeTeamLogoUrl = match.homeTeamLogoUrl,
+                        homeTeamGoals = match.homeTeamGoals,
+                        awayTeamName = match.awayTeamName,
+                        awayTeamLogoUrl = match.awayTeamLogoUrl,
+                        awayTeamGoals = match.awayTeamGoals,
+                        matchState = match.matchState
+                    )
+                }
+                Log.i("Mujtaba", "getMatch: ${_state.value}")}
+        }
+    }
+
 }
