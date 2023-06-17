5a6ef77ed9b5e016ac150f6bfce19b8496c67585
Ziad mahmoud <ziaadmahmoud2001@hotmail.com>
2023-06-17 14:43:05 +0300

    fix error in fragment discover

--- presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverViewModel.kt
@@ -1,16 +1,22 @@
 package com.cheesecake.presentation.screens.discover
 
 import android.util.Log
+import androidx.lifecycle.viewModelScope
 import com.cheesecake.domain.entity.TeamCountry
 import com.cheesecake.domain.usecases.GetSearchTeamCountryUseCase
 import com.cheesecake.domain.usecases.GetTeamCountryUseCase
 import com.cheesecake.presentation.base.BaseViewModel
 import com.cheesecake.presentation.mapper.toUIModel
+import com.cheesecake.presentation.models.TeamCountryUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,21 +27,18 @@ class DiscoverViewModel @Inject constructor(
 ) :
     BaseViewModel<DiscoverTeamCountryUIState>(DiscoverTeamCountryUIState()) {
 
-    var search = MutableStateFlow<String?>(null)
+    var searchInput = MutableStateFlow(" ")
 
     init {
         getData()
     }
 
     private fun applySearch() {
-        if (search.value != null) {
-            tryToExecute(
-                { getSearchTeamCountryUseCase(search = search.value!!) },
-                ::onSuccess,
-                ::onError
-            )
-        }
-        getData()
+        tryToExecute(
+            { getSearchTeamCountryUseCase(search = searchInput.value) },
+            ::onSuccess,
+            ::onError
+        )
 
     }
 
@@ -51,7 +54,10 @@ class DiscoverViewModel @Inject constructor(
         result.let { list ->
             _state.update { discoverTeamCountryUIState ->
                 Log.i("getData: ", list.toString())
-                discoverTeamCountryUIState.copy(data = list.map { it.toUIModel() }, isLoading = false)
+                discoverTeamCountryUIState.copy(
+                    data = list.map { it.toUIModel() },
+                    isLoading = false
+                )
             }
         }
     }
@@ -79,4 +85,8 @@ class DiscoverViewModel @Inject constructor(
             it.copy(isError = e.message.toString())
         }
     }
+
+
+
+
 }
