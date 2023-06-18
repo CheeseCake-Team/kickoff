7116364bdb91184aa1672b2a23bfdf746a130cef
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-19 00:45:02 +0300

    update adapter

--- presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverCountryAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverCountryAdapter.kt
@@ -1,10 +1,19 @@
 package com.cheesecake.presentation.screens.discover
 
-import com.cheesecake.domain.entity.TeamCountry
+import androidx.recyclerview.widget.DiffUtil
 import com.cheesecake.presentation.R
-import com.cheesecake.presentation.base.BaseAdapter
+import com.cheesecake.presentation.base.BaseListAdapter
+import com.cheesecake.presentation.models.TeamCountryUIState
 
-class DiscoverCountryAdapter:BaseAdapter<TeamCountry>(null) {
-    override val layoutId: Int
-        get() = R.layout.item_country_team
+class DiscoverCountryAdapter:BaseListAdapter<TeamCountryUIState>(TeamCountryUIStateDiffUtil()) {
+    override val layoutId = R.layout.item_country_team
+}
+
+class TeamCountryUIStateDiffUtil: DiffUtil.ItemCallback<TeamCountryUIState>() {
+    override fun areItemsTheSame(oldItem: TeamCountryUIState, newItem: TeamCountryUIState): Boolean =
+        oldItem.name == newItem.name
+
+    override fun areContentsTheSame(oldItem: TeamCountryUIState, newItem: TeamCountryUIState): Boolean =
+        oldItem == newItem
+
 }
