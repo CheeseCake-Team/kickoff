b89f2a6fe199acf9ffdd00f8f8c6eea1b6a1d1da
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-19 21:36:35 +0300

    implement MatchEventAdapter.kt

---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventAdapter.kt
@@ -0,0 +1,8 @@
+package com.cheesecake.presentation.screens.match.events
+
+import com.cheesecake.presentation.R
+import com.cheesecake.presentation.base.BaseAdapter
+
+class MatchEventAdapter: BaseAdapter<ItemEventUIState>(null) {
+    override val layoutId = R.layout.item_match_event
+}
