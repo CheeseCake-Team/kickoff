07f295276633c0fdc37ab753c6cd015869caba80
ali3030 <ali3030@users.noreply.github.com>
2023-06-19 19:27:28 +0300

    implement event ui state

--- presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventUIState.kt
@@ -3,4 +3,12 @@ package com.cheesecake.presentation.screens.match.events
 data class MatchEventUIState(
     val errorMessage: String = "error",
     val isLoading: Boolean = true,
+    val time: Int? = 0,
+    val teamId: Int= 0,
+    val playerId: Int = 0,
+    val playerName: String = "",
+    val SubstituentPlayerId: String? = "",
+    val SubstituentPlayerName: String?= "",
+    val type: String = "",
+    val detail: String? = ""
 )
