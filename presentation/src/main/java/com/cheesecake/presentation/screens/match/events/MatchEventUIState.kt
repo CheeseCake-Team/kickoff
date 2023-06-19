5c334dc43a317a657ef5a36a1cd0170b4e7831d3
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-19 21:37:21 +0300

    refactor MatchEventUIState

--- presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventUIState.kt
@@ -1,8 +1,14 @@
 package com.cheesecake.presentation.screens.match.events
 
+import com.cheesecake.domain.entity.FixtureEvents
+
 data class MatchEventUIState(
     val errorMessage: String = "error",
     val isLoading: Boolean = true,
+    val data: List<ItemEventUIState> = emptyList()
+)
+
+data class ItemEventUIState(
     val time: Int? = 0,
     val teamId: Int= 0,
     val playerId: Int = 0,
@@ -12,3 +18,16 @@ data class MatchEventUIState(
     val type: String = "",
     val detail: String? = ""
 )
+
+fun List<FixtureEvents>.toUIState(): List<ItemEventUIState>{
+    return map { ItemEventUIState(
+        time = it.time,
+        teamId = it.teamId,
+        playerId = it.playerId,
+        playerName = it.playerName,
+        SubstituentPlayerId = it.SubstituentPlayerId,
+        SubstituentPlayerName = it.SubstituentPlayerName,
+        type = it.type,
+        detail = it.detail
+    ) }
+}
