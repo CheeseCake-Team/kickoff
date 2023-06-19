0972ce93f7be23e74750762b2eb6850990f62d70
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-19 14:07:15 +0200

    add searchInput inside the ui state

--- presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverTeamCountryUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverTeamCountryUIState.kt
@@ -1,10 +1,12 @@
 package com.cheesecake.presentation.screens.discover
 
 import com.cheesecake.presentation.models.TeamCountryUIState
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
 data class DiscoverTeamCountryUIState(
     val isLoading: Boolean = true,
+    val searchInput: MutableStateFlow<String> = MutableStateFlow(""),
     val data: List<TeamCountryUIState> = emptyList(),
     val isError: String = "Error"
 )
