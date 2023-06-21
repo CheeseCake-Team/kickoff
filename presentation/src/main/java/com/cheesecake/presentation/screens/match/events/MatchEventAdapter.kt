7de869032e312f8256f20404e33a2b58eab73814
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-06-21 10:03:52 +0300

    make homeTeamId and awayTeamId private

--- presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/match/events/MatchEventAdapter.kt
@@ -8,7 +8,7 @@ import com.cheesecake.presentation.R
 import com.cheesecake.presentation.base.BaseAdapter
 import com.cheesecake.presentation.databinding.ItemMatchEventBinding
 
-class MatchEventAdapter(val homeTeamId: Int = 33,val awayTeamId: Int = 34) : BaseAdapter<ItemEventUIState>(null) {
+class MatchEventAdapter(private val homeTeamId: Int = 33, val awayTeamId: Int = 34) : BaseAdapter<ItemEventUIState>(null) {
     override val layoutId = R.layout.item_match_event
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
