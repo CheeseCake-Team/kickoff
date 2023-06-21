e2442af3f2f111346d1743148955d6d044e0dea9
shehab <sheshox12@gmail.com>
2023-06-21 15:32:35 +0300

    update database

---
+++ data/src/main/java/com/cheesecake/data/local/daos/RecentSearchDao.kt
@@ -0,0 +1,23 @@
+package com.cheesecake.data.local.daos
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.cheesecake.data.local.models.RecentSearchLocalDTO
+
+@Dao
+interface RecentSearchDao {
+
+    @Upsert
+    suspend fun updateOrInsertRecentSearch(result: RecentSearchLocalDTO)
+
+    @Query("DELETE FROM recentSearch_table WHERE id = :recentId")
+    suspend fun deleteRecentSearchById(recentId: Int)
+
+    @Query("DELETE FROM recentSearch_table")
+    suspend fun deleteRecentSearches()
+
+    @Query("SELECT * FROM recentSearch_table")
+    suspend fun getAllRecentSearches(): List<RecentSearchLocalDTO>
+
+}
--- data/src/main/java/com/cheesecake/data/local/database/KickoffDatabase.kt
+++ data/src/main/java/com/cheesecake/data/local/database/KickoffDatabase.kt
@@ -3,19 +3,23 @@ package com.cheesecake.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.cheesecake.data.local.daos.LeagueDao
+import com.cheesecake.data.local.daos.RecentSearchDao
 import com.cheesecake.data.local.daos.TeamsDao
 import com.cheesecake.data.local.models.LeagueLocalDTO
+import com.cheesecake.data.local.models.RecentSearchLocalDTO
 import com.cheesecake.data.local.models.TeamLocalDTO
 
 @Database(
-    entities = [TeamLocalDTO::class, LeagueLocalDTO::class],
+    entities = [TeamLocalDTO::class, LeagueLocalDTO::class, RecentSearchLocalDTO::class],
     version = 1
-) //TODO add all entities into the annotation
+)
 
 abstract class KickoffDatabase : RoomDatabase() {
 
     abstract fun getTeamsDao(): TeamsDao
 
     abstract fun getLeagueDao(): LeagueDao
+
+    abstract fun getSearchResultDao(): RecentSearchDao
 
 }
