b2894a9567d1c09d1386d22595eb41c7e0c5c69e
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 18:33:19 +0200

    update `GetSearchTeamCountryUseCase` duo to the refactor

--- domain/src/main/java/com/cheesecake/domain/usecases/GetSearchTeamCountryUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/GetSearchTeamCountryUseCase.kt
@@ -1,16 +1,15 @@
 package com.cheesecake.domain.usecases
 
-import com.cheesecake.domain.entity.TeamCountry
+import com.cheesecake.domain.entity.Country
 import com.cheesecake.domain.repository.IFootballRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 
 class GetSearchTeamCountryUseCase @Inject constructor(
     private val footballRepository: IFootballRepository,
 ) {
 
-    suspend operator fun invoke(search: String): Flow<List<TeamCountry>> =
+    suspend operator fun invoke(search: String): Flow<List<Country>> =
         footballRepository.getCountriesSearch(search)
 
 }
