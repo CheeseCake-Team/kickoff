826f52f60f23e0337181e943275a30f10a0f11c7
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 18:22:33 +0200

    refactor code duo to renaming

--- data/src/main/java/com/cheesecake/data/repository/mappers/CountryMapper.kt
+++ data/src/main/java/com/cheesecake/data/repository/mappers/CountryMapper.kt
@@ -2,39 +2,39 @@ package com.cheesecake.data.repository.mappers
 
 import com.cheesecake.data.local.models.TeamCountriesLocalDTO
 import com.cheesecake.data.remote.models.TeamCountriesDTO
-import com.cheesecake.domain.entity.TeamCountry
+import com.cheesecake.domain.entity.Country
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 @JvmName("teamCountryDTOToTeamCountry")
-fun TeamCountriesDTO.toEntity(): TeamCountry =
-    TeamCountry(
+fun TeamCountriesDTO.toEntity(): Country =
+    Country(
         name = name,
         flag = flag ?: ""
     )
 
-fun TeamCountriesLocalDTO.toEntity(): TeamCountry =
-    TeamCountry(
+fun TeamCountriesLocalDTO.toEntity(): Country =
+    Country(
         name = name,
         flag = flagImageUrl
     )
 
 @JvmName("teamCountryToTeamCountryLocalDTO")
-fun TeamCountry.toLocal(): TeamCountriesLocalDTO = TeamCountriesLocalDTO(
+fun Country.toLocal(): TeamCountriesLocalDTO = TeamCountriesLocalDTO(
     name = name,
     flagImageUrl = flag
 )
 
 @JvmName("listOfTeamCountryDTOToListOfTeamCountryLocalDTO")
-fun List<TeamCountry>.toLocal(): List<TeamCountriesLocalDTO> = map { it.toLocal() }
+fun List<Country>.toLocal(): List<TeamCountriesLocalDTO> = map { it.toLocal() }
 
 @JvmName("listOfTeamCountryDTOToListOfTeamCountry")
-fun List<TeamCountriesDTO>.toEntity(): List<TeamCountry> = map { it.toEntity() }
+fun List<TeamCountriesDTO>.toEntity(): List<Country> = map { it.toEntity() }
 
 @JvmName("listOfTeamCountryLocalDTOToListOfTeamCountry")
-fun List<TeamCountriesLocalDTO>.toEntity(): List<TeamCountry> = map { it.toEntity() }
+fun List<TeamCountriesLocalDTO>.toEntity(): List<Country> = map { it.toEntity() }
 
 
 @JvmName("flowListOfTeamCountryLocalDTOToFlowListOfTeamCountry")
-fun Flow<List<TeamCountriesLocalDTO>>.toEntity(): Flow<List<TeamCountry>> = map { it.toEntity() }
+fun Flow<List<TeamCountriesLocalDTO>>.toEntity(): Flow<List<Country>> = map { it.toEntity() }
 
