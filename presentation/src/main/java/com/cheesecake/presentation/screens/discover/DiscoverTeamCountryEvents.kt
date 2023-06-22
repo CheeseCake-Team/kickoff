87a734a71e862d5b8114f38904059814b9043ddd
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 18:32:21 +0200

    add `NavigateToCountry` data class

--- presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverTeamCountryEvents.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverTeamCountryEvents.kt
@@ -1,5 +1,5 @@
 package com.cheesecake.presentation.screens.discover
 
 sealed interface DiscoverTeamCountryEvents {
-    data class NavigateToCountry(val CountryName: String): DiscoverTeamCountryEvents
+    data class NavigateToCountry(val CountryName: String, val imageUrl: String): DiscoverTeamCountryEvents
 }
