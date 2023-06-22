d142d3133b0883eae1354c5263d4d2e1dd460193
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 18:19:21 +0200

    migrate `TeamsAdapter` to `BaseListAdapter` create `TeamUIStateDiffUtils`

--- presentation/src/main/java/com/cheesecake/presentation/screens/league/leagueTeams/TeamsAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/league/leagueTeams/TeamsAdapter.kt
@@ -1,10 +1,20 @@
 package com.cheesecake.presentation.screens.league.leagueTeams
 
 
+import androidx.recyclerview.widget.DiffUtil
 import com.cheesecake.presentation.R
-import com.cheesecake.presentation.base.BaseAdapter
+import com.cheesecake.presentation.base.BaseListAdapter
 import com.cheesecake.presentation.models.TeamUIState
 
-class TeamsAdapter : BaseAdapter<TeamUIState>(null) {
+class TeamsAdapter : BaseListAdapter<TeamUIState>(TeamUIStateDiffUtils()) {
     override val layoutId = R.layout.item_team
+}
+
+class TeamUIStateDiffUtils: DiffUtil.ItemCallback<TeamUIState>() {
+    override fun areItemsTheSame(oldItem: TeamUIState, newItem: TeamUIState): Boolean =
+        oldItem.teamName == newItem.teamName
+
+    override fun areContentsTheSame(oldItem: TeamUIState, newItem: TeamUIState): Boolean =
+        oldItem == newItem
+
 }
