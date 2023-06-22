81a6cc3277a1f9142b16a25012c59a002ad6d621
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 11:07:00 +0200

    create `CountryTeamsViewModel.kt`

---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/country/countryTeams/CountryTeamsViewModel.kt
@@ -0,0 +1,10 @@
+package com.cheesecake.presentation.screens.country.countryTeams
+
+import com.cheesecake.presentation.base.BaseViewModel
+import com.cheesecake.presentation.models.Event
+
+class CountryTeamsViewModel: BaseViewModel<CountyTeamsUIState, CountryTeamsNavigationEvent>(
+    CountyTeamsUIState(), Event()
+) {
+
+}
