1d783817759de8c1bb416d8d5e02d3b672293c00
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 18:36:43 +0200

    update CountryViewModel

--- presentation/src/main/java/com/cheesecake/presentation/screens/country/CountryViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/country/CountryViewModel.kt
@@ -1,36 +1,32 @@
 package com.cheesecake.presentation.screens.country
 
-import android.util.Log
-import androidx.lifecycle.viewModelScope
-import com.cheesecake.domain.entity.TeamCountry
-import com.cheesecake.domain.usecases.GetSearchTeamCountryUseCase
-import com.cheesecake.domain.usecases.GetTeamCountryUseCase
 import com.cheesecake.presentation.base.BaseViewModel
-import com.cheesecake.presentation.mapper.toUIModel
 import com.cheesecake.presentation.models.Event
-import com.cheesecake.presentation.models.TeamCountryUIState
+import com.cheesecake.presentation.screens.discover.CountryNavigationArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CountryViewModel @Inject constructor(
-) :
-    BaseViewModel<CountryUIState, CountryEvents>(
+    val countryNavigationArgs: CountryNavigationArgs
+) : BaseViewModel<CountryUIState, CountryEvents>(
         CountryUIState(),
         Event()
     ) {
 
+    init {
+        _state.update {
+            it.copy(
+                countryName = countryNavigationArgs.countryName,
+                imageUrl = countryNavigationArgs.imageUrl,
+                onBackClick = {onBackClick()}
+            )
+        }
+    }
 
-
-
+    private fun onBackClick() {
+        _event.update { Event(CountryEvents.NavigateBack) }
+    }
 
 }
