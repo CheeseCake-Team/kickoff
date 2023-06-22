3ad49f55ca663b557f8bc0326adf5e7fdcbf79c7
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-22 18:18:15 +0200

    remove `isFavourite` property

--- presentation/src/main/java/com/cheesecake/presentation/models/TeamUIState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/models/TeamUIState.kt
@@ -1,14 +1,12 @@
 package com.cheesecake.presentation.models
 
-import com.cheesecake.domain.entity.Team
 
 data class TeamUIState (
-    val teamId: Int,
     val teamName: String,
     val founded: Int,
     val teamCountry: String,
     val venueCapacity: Int,
     val venueName: String,
     val imageUrl: String,
-    val isFavourite: Boolean,
+    val onTeamClick: () -> Unit
 )
