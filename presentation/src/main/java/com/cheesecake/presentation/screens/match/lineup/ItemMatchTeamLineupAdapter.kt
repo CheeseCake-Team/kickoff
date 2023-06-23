109600f13f3e0edbf32eef78b252f678e55689b9
Mahmoud Elsayed <mahmoudelsayedessa@gamil.com>
2023-06-23 20:04:19 +0300

    merge team screen and fix his navigation

--- presentation/src/main/java/com/cheesecake/presentation/screens/match/lineup/ItemMatchTeamLineupAdapter.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/match/lineup/ItemMatchTeamLineupAdapter.kt
@@ -3,6 +3,6 @@ package com.cheesecake.presentation.screens.match.lineup
 import com.cheesecake.presentation.R
 import com.cheesecake.presentation.base.BaseAdapter
 
-class ItemMatchTeamLineupAdapter : BaseAdapter<PlayerItemUiState>(null) {
-    override val layoutId = R.layout.item_match_players
+class ItemMatchTeamLineupAdapter : BaseAdapter<FixtureLineupUiState>(null) {
+    override val layoutId = R.layout.item_match_team_lineup
 }
