0a8ad268c7499eb8ff5a23f9341062a8004cc2a7
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-25 17:35:31 +0200

    update `FavLeagueSelectionViewModel`

--- presentation/src/main/java/com/cheesecake/presentation/screens/favLeaguesSelection/FavLeagueSelectionViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/favLeaguesSelection/FavLeagueSelectionViewModel.kt
@@ -1,149 +1,98 @@
 package com.cheesecake.presentation.screens.favLeaguesSelection
 
-import android.util.Log
-import androidx.lifecycle.viewModelScope
 import com.cheesecake.domain.entity.League
 import com.cheesecake.domain.usecases.AddFavouriteLeagueListUseCase
 import com.cheesecake.domain.usecases.GetLeagueBySearchUseCase
 import com.cheesecake.domain.usecases.GetLeagueListUseCase
 import com.cheesecake.presentation.base.BaseViewModel
 import com.cheesecake.presentation.mapper.toFavLeagueItemUIState
-import com.cheesecake.presentation.mapper.toLeague
 import com.cheesecake.presentation.models.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavLeagueSelectionViewModel @Inject constructor(
     private val GetLeagueListUseCase: GetLeagueListUseCase,
-    private val AddFavouriteLeagueListUseCase: AddFavouriteLeagueListUseCase,
+    private val addFavouriteLeagueListUseCase: AddFavouriteLeagueListUseCase,
     private val getLeagueListBySearch: GetLeagueBySearchUseCase
 ) : BaseViewModel<FavLeagueSelectionUIState, FavLeagueSelectionNavigationEvent>(
     FavLeagueSelectionUIState(),
     Event()
 ) {
 
-    private val searchInput = MutableStateFlow(_state.value.searchInput)
-    private val _favLeagueSelectionUIState = MutableStateFlow(FavLeagueSelectionUIState())
-    val favLeagueSelectionUIState = _favLeagueSelectionUIState.asStateFlow()
-
     init {
-        getAllLeagues()
-        onClickSearch()
-    }
-
-    private fun getAllLeagues() {
-        tryToExecute({ GetLeagueListUseCase() }, ::onLeaguesSuccess, ::onLeagueError)
-    }
-
-    private fun onLeagueError(e: Throwable) {
-        _favLeagueSelectionUIState.update {
-            it.copy(errorMessage = e.message.toString(), isLoading = false)
+        collectFlow(state.value.searchInput.debounce(1000).distinctUntilChanged()) {
+            if (it.isBlank() || it.isEmpty()) {
+                tryToExecute({ GetLeagueListUseCase() }, ::onLeaguesSuccess, ::onLeagueError)
+            } else {
+                tryToExecute({ getLeagueListBySearch(it) }, ::onSearchSuccess, ::onSearchError)
+            }
+            state.value
         }
     }
 
     private fun onLeaguesSuccess(leagues: List<League>) {
-        _favLeagueSelectionUIState.update {
+        _state.update {
             it.copy(
                 leaguesItems = leagues.map { league ->
-                    league.toFavLeagueItemUIState { onFavouriteLeagueSelect(league.leagueId) }
+                    league.toFavLeagueItemUIState { onFavouriteLeagueSelect(league) }
                 },
-                isLoading = false
+                isLoading = false,
+                onNextClick = { addToFavourite() },
+                onSkipClick = { onSkipClick() }
             )
         }
     }
 
-    private fun onFavouriteLeagueSelect(leagueId: Int) {
-        _favLeagueSelectionUIState.update { favLeagueSelectionUIState ->
-            favLeagueSelectionUIState.selectedLeagues.find { it.leagueId == leagueId }?.let {
-                val index = favLeagueSelectionUIState.selectedLeagues.indexOf(it)
-                favLeagueSelectionUIState.selectedLeagues.removeAt(index)
-                it.copy(isFavourite = false)
-            } ?: run {
-                val league = favLeagueSelectionUIState.leaguesItems.find { it.leagueId == leagueId }
-                favLeagueSelectionUIState.selectedLeagues.add(
-                    league?.toLeague()!!.copy(isFavourite = true)
-                )
-            }
-            favLeagueSelectionUIState.copy(
-                selectedLeagues = favLeagueSelectionUIState.selectedLeagues,
-            )
+    private fun onLeagueError(e: Throwable) {
+        _state.update {
+            it.copy(errorMessage = e.message.toString(), isLoading = false)
         }
     }
 
-    fun addToFavourite() {
-        tryToExecute(
-            {
-                _favLeagueSelectionUIState.collect {
-                    AddFavouriteLeagueListUseCase(it.selectedLeagues)
+    private fun onFavouriteLeagueSelect(league: League) {
+        addFavouriteLeagueListUseCase.addLeague(league)
+        _state.update { favLeagueSelectionUIState ->
+             favLeagueSelectionUIState.copy (
+                leaguesItems = favLeagueSelectionUIState.leaguesItems.map {
+                    if (it.leagueId == league.leagueId) it.copy (isSelected = !it.isSelected) else it
                 }
+            )
+        }
+    }
 
-            }, ::onAddSuccess, ::onError
-        )
+    private fun addToFavourite() {
+        tryToExecute({ addFavouriteLeagueListUseCase.execute() }, ::onAddSuccess, ::onError)
     }
 
     private fun onAddSuccess(boolean: Boolean) {
         _event.update { Event(FavLeagueSelectionNavigationEvent.NavigateToFavTeamsSelection) }
     }
 
+    private fun onSkipClick() {
+        _event.update { Event(FavLeagueSelectionNavigationEvent.NavigateToFavTeamsSelection) }
+    }
+
     private fun onError(e: Throwable) {
-        _state.update {
-            it.copy(errorMessage = e.localizedMessage ?: "Unknown error.", isLoading = false)
-        }
-    }
-
-    private fun onClickSearch() {
-        viewModelScope.launch {
-            searchInput.debounce(1000).distinctUntilChanged().filter { it.isNotEmpty() }
-                .collect(::tryToSearch)
-        }
-    }
-
-    private suspend fun tryToSearch(input: String) {
-        tryToExecute(
-            { getSearchResult(input) }, ::onSearchSuccess, ::onSearchError
-        )
-    }
-
-    private suspend fun getSearchResult(input: String) : List<League> {
-        _favLeagueSelectionUIState.update { it.copy(isLoading = true) }
-        return getLeagueListBySearch(input)
+        _state.update { it.copy(errorMessage = e.localizedMessage ?: "Unknown error.", isLoading = false) }
     }
 
     private fun onSearchSuccess(items: List<League>) {
-        val searchItems = items.map { league ->
-            league.toFavLeagueItemUIState { onFavouriteLeagueSelect(league.leagueId) }
-        }
-        _favLeagueSelectionUIState.update {
+        _state.update {
             it.copy(
-                leaguesItems = searchItems,
+                leaguesItems = items.map { league ->
+                    league.toFavLeagueItemUIState { onFavouriteLeagueSelect(league) }
+                },
                 isLoading = false
             )
         }
     }
 
     private fun onSearchError(throwable: Throwable) {
-        _favLeagueSelectionUIState.update {
-            it.copy(
-                errorMessage = throwable.message.toString(),
-                isLoading = false
-            )
-        }
-        Log.i("onSearchError: ", _favLeagueSelectionUIState.value.errorMessage)
-    }
-
-    fun onQueryChange(input: String) {
-        viewModelScope.launch {
-            _favLeagueSelectionUIState.update { it.copy(searchInput = input) }
-            searchInput.emit(input)
-        }
+        _state.update { it.copy(errorMessage = throwable.message.toString(), isLoading = false) }
     }
 
 }
