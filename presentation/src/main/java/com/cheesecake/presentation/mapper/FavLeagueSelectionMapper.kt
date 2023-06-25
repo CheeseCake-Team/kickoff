77a90f1fe9425f257c6d2e87829f6324704cf86a
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-25 17:32:04 +0200

    refactor mapper remove unused attributes

--- presentation/src/main/java/com/cheesecake/presentation/mapper/FavLeagueSelectionMapper.kt
+++ presentation/src/main/java/com/cheesecake/presentation/mapper/FavLeagueSelectionMapper.kt
@@ -11,27 +11,7 @@ fun League.toFavLeagueItemUIState(
         leagueId = this.leagueId,
         imageUrl = this.imageUrl,
         leagueName = this.name,
-        season = this.season,
-        country = this.countryName,
-        typeName = this.leagueTypeName,
-        seasonStartYear = this.seasonStartYear,
-        seasonEndYear = this.seasonEndYear,
         isSelected = this.isFavourite,
-        onFavorite = onFavoriteLeagueClick
-    )
-}
-
-fun FavLeagueItemUIState.toLeague(): League{
-    return League(
-        leagueId = this.leagueId,
-        imageUrl = this.imageUrl,
-        name = this.leagueName,
-        season = this.season,
-        countryName = this.country,
-        leagueTypeName = this.typeName,
-        seasonStartYear = this.seasonStartYear,
-        seasonEndYear = this.seasonEndYear,
-        leagueCount = this.leagueCount,
-        isFavourite = this.isSelected
+        onFavorite = { onFavoriteLeagueClick(this.leagueId) }
     )
 }
