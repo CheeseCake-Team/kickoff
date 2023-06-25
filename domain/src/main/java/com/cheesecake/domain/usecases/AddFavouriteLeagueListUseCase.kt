304a9c1e07d650304e2762510570c227bc3872b4
Abdurrahman <abdosalahas.01128@gmail.com>
2023-06-25 17:34:48 +0200

    add execute method replace invoke with addLeague

--- domain/src/main/java/com/cheesecake/domain/usecases/AddFavouriteLeagueListUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/AddFavouriteLeagueListUseCase.kt
@@ -8,8 +8,18 @@ class AddFavouriteLeagueListUseCase @Inject constructor(
     private val leagueRepository: IFootballRepository
 ) {
 
-    suspend operator fun invoke(leagues: List<League>) : Boolean {
-        leagueRepository.addLeagueList(leagues)
+    private val selectedLeagues: MutableList<League> = mutableListOf()
+
+    fun addLeague(league: League) {
+        selectedLeagues.find { it == league }?.let {
+            selectedLeagues.remove(league.copy(isFavourite = false))
+        } ?: run {
+            selectedLeagues.add(league.copy(isFavourite = true))
+        }
+    }
+
+    suspend fun execute(): Boolean {
+        leagueRepository.addLeagueList(selectedLeagues)
         return true
     }
 
