81bf14fd0a62499c54ab279405578c3ad6d2ce6b
Abdurrahman <abdosalahas.01128@gmail.com>
2024-07-12 14:26:14 +0300

    clean up discover feature

--- domain/src/main/java/com/cheesecake/domain/usecases/ManageCountriesUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/ManageCountriesUseCase.kt
@@ -2,16 +2,24 @@ package com.cheesecake.domain.usecases
 
 import com.cheesecake.domain.entity.Country
 import com.cheesecake.domain.repository.IFootballRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCountriesUseCase @Inject constructor(
+class ManageCountriesUseCase @Inject constructor(
     private val footballRepository: IFootballRepository,
 ) {
-
-    suspend operator fun invoke(): List<Country> =
+    /**
+     * @author Ziad & Abdurrahman
+     * */
+    suspend fun getCountries(): List<Country> =
         footballRepository.getLocalCountries().takeIf { it.isNotEmpty() }
             ?: footballRepository.getRemoteCountries().also {
                 footballRepository.updateOrInsertCountries(it)
             }
 
+    /**
+     * @author Ziad
+     * */
+    suspend fun searchForCountries(countryName: String): Flow<List<Country>> =
+        footballRepository.getCountriesSearch(countryName)
 }
--- presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/discover/DiscoverViewModel.kt
@@ -1,20 +1,18 @@
 package com.cheesecake.presentation.screens.discover
 
 import com.cheesecake.domain.entity.Country
-import com.cheesecake.domain.usecases.GetSearchTeamCountryUseCase
-import com.cheesecake.domain.usecases.GetCountriesUseCase
+import com.cheesecake.domain.usecases.ManageCountriesUseCase
 import com.cheesecake.presentation.base.BaseViewModel
 import com.cheesecake.presentation.mapper.toUIModel
 import com.cheesecake.presentation.models.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class DiscoverViewModel @Inject constructor(
-    private val getCountriesUseCase: GetCountriesUseCase,
-    private val getSearchTeamCountryUseCase: GetSearchTeamCountryUseCase,
+    private val manageCountriesUseCase: ManageCountriesUseCase,
 ) : BaseViewModel<DiscoverTeamCountryUIState, DiscoverTeamCountryEvents>(
     DiscoverTeamCountryUIState(),
     Event()
@@ -25,13 +23,17 @@ class DiscoverViewModel @Inject constructor(
     }
 
     private fun applySearch(searchQuery: String) {
-        tryToExecute({ getSearchTeamCountryUseCase(searchQuery) }, ::onSearchSuccess, ::onError)
+        tryToExecute(
+            { manageCountriesUseCase.searchForCountries(searchQuery) },
+            ::onSearchSuccess,
+            ::onError
+        )
     }
 
     private fun getData() {
         collectFlow(state.value.searchInput) {
             if (it.isBlank() || it.isEmpty()) {
-                tryToExecute({ getCountriesUseCase() }, ::onSuccess, ::onError)
+                tryToExecute({ manageCountriesUseCase.getCountries() }, ::onSuccess, ::onError)
             } else {
                 applySearch(it)
             }
@@ -55,11 +57,12 @@ class DiscoverViewModel @Inject constructor(
         _event.update { Event(DiscoverTeamCountryEvents.NavigateToCountry(countryName, imageUrl)) }
     }
 
-
     private fun onSearchSuccess(flow: Flow<List<Country>>) {
         collectFlow(flow) { list ->
-            copy(data = list.map { it.toUIModel{ ::onClick.invoke(it.name, it.flag) } },
-                isLoading = false, isNoResult = list.isEmpty())
+            copy(
+                data = list.map { it.toUIModel { ::onClick.invoke(it.name, it.flag) } },
+                isLoading = false, isNoResult = list.isEmpty()
+            )
         }
     }
 
@@ -68,5 +71,4 @@ class DiscoverViewModel @Inject constructor(
             it.copy(isError = e.message.toString())
         }
     }
-
 }
