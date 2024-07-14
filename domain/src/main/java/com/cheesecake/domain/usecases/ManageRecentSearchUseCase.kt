16cd1793e656912973389a5b882d1d8e996b938f
Abdurrahman <abdosalahas.01128@gmail.com>
2024-07-14 20:52:33 +0300

    fix recent search bug

--- domain/src/main/java/com/cheesecake/domain/usecases/ManageRecentSearchUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/ManageRecentSearchUseCase.kt
@@ -4,8 +4,11 @@ import com.cheesecake.domain.entity.RecentSearch
 import com.cheesecake.domain.repository.IFootballRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.firstOrNull
+import javax.inject.Inject
 
-class ManageRecentSearchUseCase(private val repository: IFootballRepository) {
+class ManageRecentSearchUseCase @Inject constructor(
+    private val repository: IFootballRepository
+) {
     suspend fun addOrUpdateRecentSearch(recent: RecentSearch) {
         val list = repository.getRecentSearches().firstOrNull()
         if (list?.any { it.id == recent.id } == true) {
