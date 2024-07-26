9ba6aca267f7252710fcf5da0619c43d15c746bc
Abdurrahman <abdosalahas.01128@gmail.com>
2024-07-26 23:56:19 +0300

    add 'Error' data class

--- data/src/main/java/com/cheesecake/data/remote/response/BasePagingResponse.kt
+++ data/src/main/java/com/cheesecake/data/remote/response/BasePagingResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class BasePagingResponse<T>(
     @SerializedName("errors")
-    val errors: List<String>,
+    val error: Error,
     @SerializedName("paging")
     val paging: Paging,
     @SerializedName("response")
@@ -18,5 +18,12 @@ data class BasePagingResponse<T>(
         val current: Int,
         @SerializedName("total")
         val total: Int
+    )
+
+    data class Error(
+        @SerializedName("rateLimit")
+        val rateLimit: String?,
+        @SerializedName("Ip")
+        val ip: String?
     )
 }
