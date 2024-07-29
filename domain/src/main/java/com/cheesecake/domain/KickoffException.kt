9cd9e7a0c29741c0731a46de9cc739c155b89e94
Abdurrahman <abdosalahas.01128@gmail.com>
2024-07-29 03:11:27 +0300

    add new exceptions

--- domain/src/main/java/com/cheesecake/domain/KickoffException.kt
+++ domain/src/main/java/com/cheesecake/domain/KickoffException.kt
@@ -6,4 +6,9 @@ sealed class KickoffException(message: String) : Exception(message) {
     class NoInternetConnectionException : KickoffException("No internet connection")
     class InternalServerErrorException : KickoffException("Internal server error")
     class NullPointerOccur: KickoffException("Null Pointer")
+    class DomainNotAllowedException(message: String) : Exception(message)
+    class IpNotAllowedException(message: String) : Exception(message)
+    class RateLimitExceededMinuteException(message: String) : Exception(message)
+    class RateLimitExceededDayException(message: String) : Exception(message)
+    class UnknownErrorException(message: String) : Exception(message)
 }
