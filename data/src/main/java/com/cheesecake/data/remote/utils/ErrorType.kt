f878209439f82b004b1d7a1775282fabb9c441f6
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-03 15:12:53 +0300

    update errors' messages

--- data/src/main/java/com/cheesecake/data/remote/utils/ErrorType.kt
+++ data/src/main/java/com/cheesecake/data/remote/utils/ErrorType.kt
@@ -3,7 +3,7 @@ package com.cheesecake.data.remote.utils
 enum class ErrorType(val message: String) {
     DOMAIN_NOT_ALLOWED("This Domain is not allowed to call the API, check the list of allowed Domains in the dashboard."),
     IP_NOT_ALLOWED("This IP is not allowed to call the API, check the list of allowed IPs in the dashboard."),
-    RATE_LIMIT_EXCEEDED_MINUTE("Too many requests. You have exceeded the limit of requests per minute of your subscription."),
-    RATE_LIMIT_EXCEEDED_DAY("Too many requests. You have exceeded the limit of requests per day of your subscription."),
+    RATE_LIMIT_EXCEEDED_MINUTE("Too many requests. Your rate limit is 10 requests per minute."),
+    RATE_LIMIT_EXCEEDED_DAY("Too many requests. Your rate limit is 100 requests per day."),
     UNKNOWN_ERROR("An unknown error occurred.")
 }
