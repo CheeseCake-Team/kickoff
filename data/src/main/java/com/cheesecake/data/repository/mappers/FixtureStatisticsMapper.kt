7dd623036183f52953041fd1090daf67273998d3
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-14 08:48:42 +0300

    fix no data at match statistics bug

--- data/src/main/java/com/cheesecake/data/repository/mappers/FixtureStatisticsMapper.kt
+++ data/src/main/java/com/cheesecake/data/repository/mappers/FixtureStatisticsMapper.kt
@@ -7,12 +7,12 @@ import com.cheesecake.domain.entity.StatisticsType
 @JvmName("FixtureStatisticsDTOToFixtureStatistics")
 fun List<FixtureStatisticsDTO>.toEntity(): List<FixtureStatistics> {
     if (this.isEmpty())
-        throw Exception()
-    return this[0].statistics.indices.map { i ->
-        val type = this[0].statistics[i].type
+        return emptyList()
+    return this.first().statistics.indices.map { i ->
+        val type = this.first().statistics[i].type
         val statisticsType = toStatisticsType(type)
-        val homeTeamValue = this[0].statistics[i].value.percentageToInt()
-        val awayTeamValue = this[1].statistics[i].value.percentageToInt()
+        val homeTeamValue = this.first().statistics[i].value.percentageToInt()
+        val awayTeamValue = this.first().statistics[i].value.percentageToInt()
 
         FixtureStatistics(type, statisticsType, homeTeamValue, awayTeamValue)
     }
