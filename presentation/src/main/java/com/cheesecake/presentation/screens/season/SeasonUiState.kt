24444f5ec43d9ad5aa5d1e6d8b297e66d3a73f08
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-22 01:59:10 +0300

    update `SeasonUiState`

--- presentation/src/main/java/com/cheesecake/presentation/screens/season/SeasonUiState.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/season/SeasonUiState.kt
@@ -2,5 +2,5 @@ package com.cheesecake.presentation.screens.season
 
 data class SeasonUiState(
     val selectedSeason: String = "",
-    val seasonItemUiState: List<SeasonItemUiState> = emptyList()
+    val seasonItemsUiState: List<SeasonItemUiState> = emptyList()
 )
