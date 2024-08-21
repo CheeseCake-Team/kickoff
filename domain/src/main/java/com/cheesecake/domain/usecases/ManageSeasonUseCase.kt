dca3aaac1e751090509a0815730b8b824cae875b
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-22 02:00:57 +0300

    update use case

--- domain/src/main/java/com/cheesecake/domain/usecases/ManageSeasonUseCase.kt
+++ domain/src/main/java/com/cheesecake/domain/usecases/ManageSeasonUseCase.kt
@@ -7,10 +7,14 @@ import javax.inject.Inject
 class ManageSeasonUseCase @Inject constructor(
     private val repository: IFootballRepository
 ) {
-
     fun getSeason(): Flow<String> = repository.getSeason()
 
-    suspend fun setSeason(season: String) {
+    suspend fun setSeason(season: String): String {
         repository.setSeason(season)
+        return season
+    }
+
+    suspend fun getAllSeasons(): List<String> {
+        return repository.getAllSeasons()
     }
 }
