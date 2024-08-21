99183852131af91a78e1c821d212e79a2dd3b4cd
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-22 02:03:53 +0300

    implement `SettingsViewModel`

--- presentation/src/main/java/com/cheesecake/presentation/screens/settings/SettingsViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/settings/SettingsViewModel.kt
@@ -1,9 +1,27 @@
 package com.cheesecake.presentation.screens.settings
 
-import androidx.lifecycle.ViewModel
+import com.cheesecake.domain.usecases.ManageSeasonUseCase
+import com.cheesecake.presentation.base.BaseViewModel
+import com.cheesecake.presentation.models.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel: ViewModel() {
+class SettingsViewModel @Inject constructor(
+    private val manageSeasonUseCase: ManageSeasonUseCase
+) : BaseViewModel<SettingsUiState, SettingsEvent>(SettingsUiState(), Event()) {
+    init {
+        getData()
+    }
 
+    override fun getData() {
+        collectFlow(manageSeasonUseCase.getSeason()) { season ->
+            copy(selectedSeason = season)
+        }
+    }
+
+    fun onSeasonClicked() {
+        _event.update { Event(SettingsEvent.OnSeasonClicked) }
+    }
 }
