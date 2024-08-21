b642916b1bca607521362611495c599468c23088
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-21 06:54:35 +0300

    create `settingsViewModel`

---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/settings/SettingsViewModel.kt
@@ -0,0 +1,9 @@
+package com.cheesecake.presentation.screens.settings
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SettingsViewModel: ViewModel() {
+
+}
