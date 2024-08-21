63ce2cafce841bce7d980ef28f85f8c5ad79cf25
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-21 20:24:13 +0300

    create `SeasonViewModel`

---
+++ presentation/src/main/java/com/cheesecake/presentation/screens/season/SeasonViewModel.kt
@@ -0,0 +1,8 @@
+package com.cheesecake.presentation.screens.season
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SeasonViewModel: ViewModel() {
+}
