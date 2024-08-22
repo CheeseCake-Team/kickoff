85b330227c517043fb3c6f0655b75b9d9a9fc378
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-22 03:11:35 +0300

    bind timezone data source

--- app/src/main/java/com/cheesecake/kickoff/di/DataSourceModule.kt
+++ app/src/main/java/com/cheesecake/kickoff/di/DataSourceModule.kt
@@ -3,11 +3,13 @@ package com.cheesecake.kickoff.di
 import com.cheesecake.data.local.LocalDataSourceImp
 import com.cheesecake.data.local.OnboardingDataSourceImpl
 import com.cheesecake.data.local.SeasonLocalDataSourceImpl
+import com.cheesecake.data.local.TimezoneLocalDataSourceImpl
 import com.cheesecake.data.remote.RemoteDataSourceImp
 import com.cheesecake.data.repository.LocalDataSource
 import com.cheesecake.data.repository.OnboardingDataSource
 import com.cheesecake.data.repository.RemoteDataSource
 import com.cheesecake.data.repository.SeasonLocalDataSource
+import com.cheesecake.data.repository.TimezoneLocalDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -27,4 +29,7 @@ abstract class DataSourceModule {
 
     @Binds
     abstract fun bindSeasonDataSource(seasonLocalDataSourceImpl: SeasonLocalDataSourceImpl): SeasonLocalDataSource
+
+    @Binds
+    abstract fun bindTimezoneDataSource(timezoneLocalDataSourceImpl: TimezoneLocalDataSourceImpl): TimezoneLocalDataSource
 }
