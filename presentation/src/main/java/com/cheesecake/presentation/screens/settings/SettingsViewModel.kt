e65d1cb15ac56564ab85d02961f1e917459267db
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-22 12:48:41 +0300

    update `SettingsViewModel`

--- presentation/src/main/java/com/cheesecake/presentation/screens/settings/SettingsViewModel.kt
+++ presentation/src/main/java/com/cheesecake/presentation/screens/settings/SettingsViewModel.kt
@@ -1,6 +1,7 @@
 package com.cheesecake.presentation.screens.settings
 
 import com.cheesecake.domain.usecases.ManageSeasonUseCase
+import com.cheesecake.domain.usecases.ManageTimezoneUseCase
 import com.cheesecake.presentation.base.BaseViewModel
 import com.cheesecake.presentation.models.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,7 +10,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val manageSeasonUseCase: ManageSeasonUseCase
+    private val manageSeasonUseCase: ManageSeasonUseCase,
+    private val manageTimezoneUseCase: ManageTimezoneUseCase
 ) : BaseViewModel<SettingsUiState, SettingsEvent>(SettingsUiState(), Event()) {
     init {
         getData()
@@ -19,9 +21,16 @@ class SettingsViewModel @Inject constructor(
         collectFlow(manageSeasonUseCase.getSeason()) { season ->
             copy(selectedSeason = season)
         }
+        collectFlow(manageTimezoneUseCase.getTimezone()) { timezone ->
+            copy(selectedTimeZone = timezone)
+        }
     }
 
     fun onSeasonClicked() {
         _event.update { Event(SettingsEvent.OnSeasonClicked) }
+    }
+
+    fun onTimezoneClicked() {
+        _event.update { Event(SettingsEvent.OnTimezoneClicked) }
     }
 }
