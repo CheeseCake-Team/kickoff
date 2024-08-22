329a6b7417419e5c9edeca6f73895d38202b867d
Abdurrahman <abdosalahas.01128@gmail.com>
2024-08-22 03:14:09 +0300

    create and implement `ManageTimezoneUseCase`

---
+++ domain/src/main/java/com/cheesecake/domain/usecases/ManageTimezoneUseCase.kt
@@ -0,0 +1,21 @@
+package com.cheesecake.domain.usecases
+
+import com.cheesecake.domain.repository.IFootballRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ManageTimezoneUseCase @Inject constructor(
+    private val repository: IFootballRepository
+) {
+    suspend fun getTimezones(): List<String> {
+        return repository.getTimezones()
+    }
+
+    fun getTimezone(): Flow<String> {
+        return repository.getTimezone()
+    }
+
+    suspend fun setTimezone(timezone: String) {
+        return repository.setTimezone(timezone)
+    }
+}
